60f96c6890e8494e6bc041c0c64a63303ab4ca91
Bas Westerbaan <bas@fsfe.org>
2007-11-29 22:52:13 +0100

    Forum link
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- lib/common.php
+++ lib/common.php
@@ -63,7 +63,7 @@ function default_footer() { ?>
 					<li><a href="http://www.flickr.com/photos/12267979@N04/">Karpe Noktem</a></li>
 					<li><a href="http://www.flickr.com/photos/12264801@N04/">Pluk het weekend</a></li>
 				</ul>
-				<li>Forum</li>
+				<li><a href="http://www.ru.nl/karpenoktem/forum">Forum</a></li>
 				<li><a href="<?php echo auri('merchandise') ?>"
 						>Merchandise</a></li>
 				<ul>
