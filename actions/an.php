54dcc65532622a801260440e0a0a1ae9cf00f1bc
Bas Westerbaan <bas@fsfe.org>
2007-12-03 20:42:22 +0100

    Links op de akta nokturna pagina.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/an.php
+++ actions/an.php
@@ -1,3 +1,22 @@
 <?php default_header(); ?>
-	<p>Spoedig zul je hier de oude Akta Nokturna's kunnen downloaden.</p>
+	<h2>Akta Nokturna</h2>
+	<p>Hier kunt u de oudere uitgaven van de Akta Nokturna downloaden</p>
+	<ul>
+		<li>Jaargang 1</li>
+		<ul>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 1</a></li>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 2</a></li>
+		</ul>
+		<li>Jaargang 2</li>
+		<ul>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 3</a></li>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 4</a></li>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 5</a></li>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 6</A></li>
+		</ul>
+		<li>Jaargang 3</li>
+		<ul>
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 7</a></li>
+		</ul>
+	</ul>	
 <?php default_footer(); ?>
