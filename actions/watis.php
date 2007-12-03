071998fcf44339f916f36b4adaf6aacf271eb99d
Bas Westerbaan <bas@fsfe.org>
2007-12-03 19:21:09 +0100

    Titel voor watis pagina.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/watis.php
+++ actions/watis.php
@@ -1,4 +1,5 @@
 <?php default_header(); ?>
+<h2>Wat is Karpe Noktem?</h2>
 <img src="<?php echo curi('img/dansend.jpg'); ?>" class="right" />
 <p>Karpe Noktem is een dynamisch alternatief voor iedereen die zich niet tot 
 traditionele verenigingen aangetrokken voelt. Wij brengen HBO- en WO-studenten
