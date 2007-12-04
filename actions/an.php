6db70c52e9f8d2eb5973e0d863d31a15dc388240
Bas Westerbaan <bas@fsfe.org>
2007-12-04 15:18:53 +0100

    Volgorde op Akta Nokturna pagina omgekeerd.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/an.php
+++ actions/an.php
@@ -2,10 +2,15 @@
 	<h2>Akta Nokturna</h2>
 	<p>Hier kunt u de oudere uitgaven van de Akta Nokturna downloaden</p>
 	<ul>
-		<li>Jaargang 1</li>
+		<li>Jaargang 4</li>
 		<ul>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 1</a></li>
-			<li><a href="<?php echo curi('archief/akta2.pdf'); ?>">Akta 2</a></li>
+			<li><a href="<?php echo curi('archief/akta10.pdf'); ?>">Akta 10</a></li>
+		</ul>	
+		<li>Jaargang 3</li>
+		<ul>	
+			<li><a href="<?php echo curi('archief/akta7.pdf'); ?>">Akta 7</a></li>
+			<li><a href="<?php echo curi('archief/akta8.pdf'); ?>">Akta 8</a></li>
+			<li><a href="<?php echo curi('archief/akta9.pdf'); ?>">Akta 9</a></li>
 		</ul>
 		<li>Jaargang 2</li>
 		<ul>
@@ -14,15 +19,10 @@
 			<li><a href="<?php echo curi('archief/akta5.pdf'); ?>">Akta 5</a></li>
 			<li><a href="<?php echo curi('archief/akta6.pdf'); ?>">Akta 6</A></li>
 		</ul>
-		<li>Jaargang 3</li>
-		<ul>	
-			<li><a href="<?php echo curi('archief/akta7.pdf'); ?>">Akta 7</a></li>
-			<li><a href="<?php echo curi('archief/akta8.pdf'); ?>">Akta 8</a></li>
-			<li><a href="<?php echo curi('archief/akta9.pdf'); ?>">Akta 9</a></li>
-		</ul>
-		<li>Jaargang 4</li>
+		<li>Jaargang 1</li>
 		<ul>
-			<li><a href="<?php echo curi('archief/akta10.pdf'); ?>">Akta 10</a></li>
-		</ul>	
+			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 1</a></li>
+			<li><a href="<?php echo curi('archief/akta2.pdf'); ?>">Akta 2</a></li>
+		</ul>
 	</ul>	
 <?php default_footer(); ?>
