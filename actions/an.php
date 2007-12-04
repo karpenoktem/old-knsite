9dbd5e34269d8a47d21008daf737696f64e507f3
Bas Westerbaan <bas@fsfe.org>
2007-12-04 15:10:21 +0100

    Fix van akta pagina en akta 8.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/an.php
+++ actions/an.php
@@ -5,18 +5,20 @@
 		<li>Jaargang 1</li>
 		<ul>
 			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 1</a></li>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 2</a></li>
+			<li><a href="<?php echo curi('archief/akta2.pdf'); ?>">Akta 2</a></li>
 		</ul>
 		<li>Jaargang 2</li>
 		<ul>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 3</a></li>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 4</a></li>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 5</a></li>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 6</A></li>
+			<li><a href="<?php echo curi('archief/akta3.pdf'); ?>">Akta 3</a></li>
+			<li><a href="<?php echo curi('archief/akta4.pdf'); ?>">Akta 4</a></li>
+			<li><a href="<?php echo curi('archief/akta5.pdf'); ?>">Akta 5</a></li>
+			<li><a href="<?php echo curi('archief/akta6.pdf'); ?>">Akta 6</A></li>
 		</ul>
 		<li>Jaargang 3</li>
 		<ul>
-			<li><a href="<?php echo curi('archief/akta1.pdf'); ?>">Akta 7</a></li>
+			
+			<li><a href="<?php echo curi('archief/akta7.pdf'); ?>">Akta 7</a></li>
+			<li><a href="<?php echo curi('archief/akta8.pdf'); ?>">Akta 8</a></li>
 		</ul>
 	</ul>	
 <?php default_footer(); ?>
