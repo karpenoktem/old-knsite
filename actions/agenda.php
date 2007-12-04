fdd3041288462244732c3a5652aa89b9cf59178f
Bas Westerbaan <bas@fsfe.org>
2007-12-04 19:46:06 +0100

    Agenda updates van 4 dec.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/agenda.php
+++ actions/agenda.php
@@ -19,7 +19,47 @@
 	<div class="desc">Een activiteit die past bij onze Corporale Commissie.
 	Voor meer info kun je terecht bij Daan Fifis!</div></li>
 	
+	<li><div class="date">Woensdag 19 december</div>
+	<div class="name">Karpe Rock'tem! in Muziekcafe Merleyn</div>
+	<div class="desc">Het studentenleven trilt op zijn grondvesten,
+	Totaalfeest Karpe Rockt'em is weer in de stad! Cancel alle afspraken
+	voor morgen maar... want we pakken uit met het live-concert van
+	<em>Mark &amp; The Spies</em> en gaan vervolgens door met de 
+	dampende set van DJ's <em>Eli</em> &amp; <em>Jesper</em>.<br/>
+	Vanaf 22:00. 3 euro entroo. Leden gratis.</div></li>
 	
+	<li><div class="date">Woensdag 2 januari</div>
+	<div class="name">Nieuwjaarsborrel in Cafe Dollars</div>
+	<div class="desc">Bijgekomen van alle nieuwsjaarsfeesten en nog zin om
+	even na te loungen? Wens alle leden een vrolijk nieuwjaar in stijl op
+	de borrel, compleet met oliebollen, goedkope champagne en een
+	nieuwjaarsconference.<br/>
+	Vanaf 20:30. <em>Open activiteit</em>.
+	Bier euro 1.40. Fris 1.10.</div></li>
+
+	<li><div class="date">Woensdag 9 januari</div>
+	<div class="name">Extreme Playbackshow</div>
+	<div class="desc">Ze zijn er allemaal groot geworden: Micheal Jackson,
+	Whitney Houston, The Venga Boys, Ariel de kleine Zeemeermin en
+	Wim Sonneveld. G Geef je over aan de Campy Cultus die de jaarlijkse
+	Extreme Playbackshow heet.<br/>
+	Vanaf 21:00. Enkel voor leden.</div></li>
+
+	<li><div class="date">Woensdag 16 januari</div>
+	<div class="name">De Grote <del>Jupiler</del> Heineken Borrel</div>
+	<div class="desc">Nog aan het leren voor de tentamens of nog gestressd
+	na afloop? Kom uitpuffen op de ouderwets gezellige borrel. Jij hoeft
+	niks te doen: wij hebben het bier koud staan.<br/>
+	Vanaf 20:30. Open activiteit. Bier euro 1.40. Fris 1.10.</div></li>
+
+	<li><div class="date">Vrijdag 25 januari</div>
+	<div class="name">Karpe Noktem Winterweekend</div>
+	<div class="desc">Dit weekend luiden we met trots een splinternieuwe
+	traditie in! Karpe Noktem Winterweekend. Minder Bier. Meer Gluhwin,
+	hete chocolademelk, ouderwetse bordspelletjes, korte momenten van
+	verdiepingen en heel veel gezelligheid.  De inschrijving gaat
+	binnenkort van start.<br/>
+	Van vrijdag 25 tot zondag 27 januari. Leden only.</div></li>
 </ul>
 
 <?php default_footer(); ?>
--- actions/default.php
+++ actions/default.php
@@ -10,6 +10,21 @@
 				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
 						CoCo: Stropdasdrinken</a></div>
 				    <div class="desc">13 Dec</div>
-				
+
+				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
+						Karpe Rockt'em</a></div>
+				    <div class="desc">19 Dec, Cafe Merleyn</div>
+				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
+						Nieuwjaarsborrel</a></div>
+				    <div class="desc">2 Jan, Cafe Dollars</div>
+				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
+						Extreme Playbackshow</a></div>
+				    <div class="desc">9 Jan</div>
+				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
+						Borrel</a></div>
+				    <div class="desc">16 Jan</div>
+				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
+						Winterweekend</a></div>
+				    <div class="desc">25 Jan</div>
 			</ul>
 <?php default_footer(); ?>
