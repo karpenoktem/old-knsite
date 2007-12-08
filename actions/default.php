8a46f3a1396d66d917dbb728842bfadad872af5b
Bas Westerbaan <bas@fsfe.org>
2007-12-08 01:12:15 +0100

    karpenoktem.nl pad behouden referer hack Karpenoktem.nl verwijst op dit moment alles door naar ru.nl/karpenoktem zonder het pad te behouden.  Nu word er naar de referer gekeken.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/default.php
+++ actions/default.php
@@ -1,4 +1,22 @@
-<?php $page['bg'] = 'bg-home.png';
+<?php 
+// KARPENOKTEM.NL UCI HOSTING HACK
+// karpenoktem.nl verwijst alle verzoeken direct naar ru.nl/karpenoktem
+// ongeacht van de uri erachter.  Zijn we op deze pagina via karpenoktem.nl
+// gekomen, verwijs dan door naar de bedoelde pagina.
+
+$ref = false;
+$ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $ref;
+$ref = isset($in['referer']) ? $in['referer'] : $ref;
+
+if($ref and 
+   preg_match('/^https?:\/\/([^\/]+)\/(.*)$/', $ref, $matches) and
+   $matches[1] == 'karpenoktem.nl') {
+	Header("location: http://ru.nl/karpenoktem/{$matches[2]}");
+	exit();
+}
+
+
+$page['bg'] = 'bg-home.png';
 	  default_header(); ?>
 			<ul id="agenda">
 				<li><div class="title"><a href="<?php echo auri('agenda'); ?>">
