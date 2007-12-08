3d9a527738590588710cfcd7f7efb87c626de3ff
Bas Westerbaan <bas@fsfe.org>
2007-12-08 16:37:12 +0100

    Punctuatie hoort geen deel te zijn van links nog een opmerking tussen haakjes.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/links.php
+++ actions/links.php
@@ -1,9 +1,9 @@
 <?php default_header(); ?>
 	<p>Karpe Noktem op</p>
 	<ul>
-		<li><a href="http://karpenoktem.hyves.nl/">Hyves,</a></li>
-		<li><a href="http://www.youtube.com/KarpeNoktem">YouTube (video's),</a></li>
-		<li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr (foto's),</a></li>
-		<li>en <a href="http://nl.wikipedia.org/wiki/Karpe_Noktem">Wikipedia.</a></li>
+		<li><a href="http://karpenoktem.hyves.nl/">Hyves</a>,</li>
+		<li><a href="http://www.youtube.com/KarpeNoktem">YouTube</a> (video's),</li>
+		<li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr (foto's)</a>,</li>
+		<li>en <a href="http://nl.wikipedia.org/wiki/Karpe_Noktem">Wikipedia</a>.</li>
 	</ul>
 <?php default_footer(); ?>
