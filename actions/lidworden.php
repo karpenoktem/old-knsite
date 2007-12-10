8cbff8635e3be8831b9e92b9d5cdc22ba8b7b4ea
Bas Westerbaan <bas@fsfe.org>
2007-12-10 13:02:21 +0100

    Op lidworden pagina e-mail van de secretaris.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lidworden.php
+++ actions/lidworden.php
@@ -2,5 +2,5 @@
 	  default_header(); ?>
 <p>Kom eens een keer langs bij een van onze 
 <a href="<?php echo curi('agenda'); ?>">activiteiten</a> of stuur een e-mailtje
-naar <?php echo email('voorzitter@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
+naar <?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
 <?php default_footer(); ?>
