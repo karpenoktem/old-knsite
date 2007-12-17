5457ef87f9906948d71c98fc235612739dd63185
Bas Westerbaan <bas@fsfe.org>
2007-12-17 15:43:01 +0100

    Zinsobouwfoutje op de Contact pagina.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/contact.php
+++ actions/contact.php
@@ -4,5 +4,5 @@
 spreekuur - alleen op afspraak, tenminste 1 dag van tevoren gemeld via
 <?php echo email('voorzitter@karpenoktem.nl'); ?> - tijdens de pauze.</p>
 <p>Voor vragen ofwel opmerkingen over karpenoktem.nl kunt u mailen met
-<?php echo email('bas@fsfe.org'); ?> wezen.</p>
+<?php echo email('bas@fsfe.org'); ?>.</p>
 <?php default_footer(); ?>
