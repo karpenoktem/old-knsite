57bc2be3c8a5e46e3ab499d3925dd8e995fb47bb
Bas Westerbaan <bas@fsfe.org>
2008-01-28 21:44:28 +0100

    (Late) agenda updates van 28 januari.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- config.agenda.php
+++ config.agenda.php
@@ -6,36 +6,16 @@ $cfg['agenda'] = array(
 	      "Datum op agenda pagina",
 	      "Titel op agenda pagina",
 	      "Tekst op agenda pagina"), */
-	array("Quizborrel in Dollars",
-	      "16 Jan",
-	      "Woensdag 16 januari",
-	      "Muziek-Quiz-Borrel",
-	      "Er zal een spetterende muziek quiz, gemaakt door onze eigen ".
-	      "voorzitter, worden gehouden in cafe Dollars.<br/>".
-	      "Cafe Dollars, vanaf 20:30."),
-	array("LoCo: Toetjesavond",
-	      "22 Jan",
-	      "Dinsdag 22 januari",
-	      "LoCo: Toetjesavond",
-	      "Dit zal de eerste Loco activiteit van dit jaar worden, de ".
-	      "toetjesavond. Maak je eigen goddelijke chocolademousse (of ".
-	      "iets anders natuurlijk) en kom om 20:30 naar gang 114 op ".
-	      "Hoogeveldt (bij Bram en Jorne thuis). Mocht je nog toetjes ".
-	      "moeten afmaken is daar gelegenheid voor.<br/>".
-	      "Gang 114, Hoogeveldt, vanaf 20:30."),
-	array("Bandjesavond",
-	      "23 Jan",
-	      "Woensdag 23 januari",
-	      "Bandjesavond in Piecken",
-	      "De line up volgt pas volgende week, maar be there or be ".
-	      "square."),
-	array("Winterweekend",
-	      "25 - 27 jan",
-	      "25 tot en met 27 januari",
-	      "Winterweekend",
-	      "Het eerst winterweekend.  Het beleooft een heel erg relaxt ".
-	      "weekend te worden, gluhwein included. Er zijn een klein ".
-	      "aantal afmeldingen, zie de leden mails."),
+	array("Muziek Quiz",
+	      "30 jan",
+	      "30 januari",
+	      "Muziek Quiz van de jaren nul, in de herkansing",
+	      "Wegens omstandigheden kon deze vorige week niet plaatsvinden, ".
+	      "maar volgende week zal deze gegarandeerd plaatsvinden. Kom ".
+	      "gezellig de tentamenstress wegwerken, drink een lekker pilsje ".
+	      "en etaleer je muziekkennis (of uiteraard het gebrek daar ".
+	      "aan).<br/>".
+	      "Cafe Dollars, vanaf 20:30"),
 	array("Lullo's marathon",
 	      "31 jan",
 	      "31 januari",
