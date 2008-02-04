7e599428542317a7c0e9f282974499a2b7fa4f6c
standaard www-gebruiker voor main <karpenok@galeras.uci.kun.nl>
2008-02-04 14:44:40 +0100

    Agenda updates van 4 februari

--- config.agenda.php
+++ config.agenda.php
@@ -12,8 +12,16 @@ $cfg['agenda'] = array(
 	      "Tweede Algemene LedenVergadering",
 	      "Het is handig om alvast te noteren dat de tweede ALV van dit ".
 	      "bestuursjaar op woensdag 13 februari zal worden gehouden. We ".
-	      "beginnen STIPT om 21:00 uur in cafe Dollars. Agenda volgt.<br/>",
-	      "Cafe Dollars, aanvang stipt 21:00")
+	      "beginnen STIPT om 21:00 uur in cafe Dollars. Agenda volgt.<br/>". 
+	      "Cafe Dollars, aanvang stipt 21:00"),
+	array("Spellenavond",
+	      "22 Feb",
+	      "Vrijdag 22 februari",
+	      "Spellenavond in cafe Piecken",
+	      "De tweede spellenavond van dit jaar. Kom je medeleden ".
+	      "uitdagen tot een potje Guitar Hero, kom lekker een potje".
+              " pokeren of ouderwets kolonisten spelen.<br/>".
+	      "Aanvang 20:30 uur.")
 );
 
 ?>
