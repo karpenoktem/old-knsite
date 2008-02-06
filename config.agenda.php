b3c1d065f387198b44982b64e1361144bfec3ec8
Bas Westerbaan <bas@fsfe.org>
2008-02-06 15:53:16 +0100

    Agenda updates voor 6 februari.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- config.agenda.php
+++ config.agenda.php
@@ -6,14 +6,30 @@ $cfg['agenda'] = array(
 	      "Datum op agenda pagina",
 	      "Titel op agenda pagina",
 	      "Tekst op agenda pagina"), */
+	array("Filmavond",
+	      "6 Feb",
+	      "Woensdag 6 februari",
+	      "Filmavond bij Daan",
+	      "De fantastische after-carnaval filmavond bij Daan thuis.".
+	      "Daan zal zorgen voor de eerste film, Die Hard 4.0, neem vooral ".
+	      "wat dvd's mee. Verder wordt het gewaardeerd zelf een drankje ".
+	      "mee te nemen, Daan is helaas geen filantropische instelling.".
+	      "<br/>St Annastraat 21 A, vanaf 21:00"),
 	array("Tweede ALV",
 	      "13 Feb",
 	      "Woensdag 13 februari",
 	      "Tweede Algemene LedenVergadering",
 	      "Het is handig om alvast te noteren dat de tweede ALV van dit ".
 	      "bestuursjaar op woensdag 13 februari zal worden gehouden. We ".
-	      "beginnen STIPT om 21:00 uur in cafe Dollars. Agenda volgt.<br/>". 
+	      "beginnen STIPT om 21:00 uur in cafe Dollars. Agenda volgt.<br/>".
 	      "Cafe Dollars, aanvang stipt 21:00"),
+	array("Dansfeest",
+	      "20 Feb",
+	      "Woensdag 20 februari",
+	      "Dansfeest",
+	      "Tijd voor een avond lekker losgaan, Karpe Noktem organiseert ".
+	      "een overweldigend dansfeest. De locatie volgt, maar een ".
+	      "geslaagd feest, inclusief huis DJ Jesper is zeker"),
 	array("Spellenavond",
 	      "22 Feb",
 	      "Vrijdag 22 februari",
@@ -21,7 +37,16 @@ $cfg['agenda'] = array(
 	      "De tweede spellenavond van dit jaar. Kom je medeleden ".
 	      "uitdagen tot een potje Guitar Hero, kom lekker een potje".
               " pokeren of ouderwets kolonisten spelen.<br/>".
-	      "Aanvang 20:30 uur.")
+	      "Aanvang 16:00 uur voor vroege vogels. 19:30 barst het los."),
+	array("Pokeravond",
+	      "5 Maart",
+	      "Woensdag 5 maart",
+	      "Pokeravond",
+	      "Voor alle Marcel Luske's, Noah Boeken's en minder goden van de ".
+	      "pokerwereld organiseert Karpe Noktem de tweede pokeravond van ".
+	      "dit jaar. Wie o wie wordt Judith's opvolg(st)er als ".
+	      "pokermeester(es)?<br/>".
+	      "Cafe 't Haantje, vanaf 20:30")
 );
 
 ?>
