37032612c93208b471a6f01d8b522646d76a65a0
Bas Westerbaan <bas@fsfe.org>
2008-02-26 14:52:41 +0100

    Agenda updates naar de ledenmail van 26 februari.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- config.agenda.php
+++ config.agenda.php
@@ -6,27 +6,14 @@ $cfg['agenda'] = array(
 	      "Datum op agenda pagina",
 	      "Titel op agenda pagina",
 	      "Tekst op agenda pagina"), */
-	array("Dansfeest",
-	      "20 Feb",
-	      "Woensdag 20 februari",
-	      "Dansfeest",
-	      "Tijd voor een avond lekker losgaan, Karpe Noktem organiseert ".
-	      "een overweldigend dansfeest. De locatie volgt, maar een ".
-	      "geslaagd feest, inclusief huis DJ Jesper is zeker"),
-	array("Spellenavond",
-	      "22 Feb",
-	      "Vrijdag 22 februari",
-	      "Spellenavond in cafe Piecken",
-	      "De tweede spellenavond van dit jaar. Kom je medeleden ".
-	      "uitdagen tot een potje Guitar Hero, kom lekker een potje".
-              " pokeren of ouderwets kolonisten spelen.<br/>".
-	      "Aanvang 16:00 uur voor vroege vogels. 19:30 barst het los."),
-	array("Themaborrel",
+	array("Jeugdsentimentborrel",
 	      "27 Feb",
 	      "Woensdag 27 februari",
-	      "Themaborrel",
-	      "Het Theme zal zo snel mogelijk bekend gemaakt worden<br/>".
-	      "Aanvang 8:30, Cafe Dollars"),
+	      "Jeugdsentiment Borrel",
+	      "Belangrijk: voor alle mensen die geinteresseerd zijn om als ".
+	      "vrijwilliger in de Barcommissie te gaan, Jorne geeft vanaf ".
+	      "21:00 uur een korte inwerkcursus<br/>".
+	      "Aanvang 20:30, Cafe Dollars"),
 	array("Pokeravond",
 	      "5 Maart",
 	      "Woensdag 5 maart",
@@ -35,7 +22,44 @@ $cfg['agenda'] = array(
 	      "pokerwereld organiseert Karpe Noktem de tweede pokeravond van ".
 	      "dit jaar. Wie o wie wordt Judith's opvolg(st)er als ".
 	      "pokermeester(es)?<br/>".
-	      "Cafe 't Haantje, vanaf 20:30")
+	      "Cafe 't Haantje, vanaf 20:30"),
+	array("Benefiet feest",
+	      "12 Maart",
+	      "Woensdag 12 maart",
+	      "Benefiet feest met Carolus",
+	      "Het grote Karpe Noktem/Carolus Magnus Tussen twee Werelden ".
+	      "benefiet feest. Dit zal plaatsvinden op de societeit van ".
+	      "Carolus (Hertogstraat 141) en om 22:00 uur beginnen. Tijdens ".
+	      "het feest zal DJ Jesper draaien en kunnen jullie genieten van ".
+	      "zowel de Carolus als de Karpe Noktem huisband. De opbrengst ".
+	      "van dit feest gaat naar het goede doel!"),
+	array("ZAK: Doornroosje",
+	      "16 Maart",
+	      "Zondag 16 Maart",
+	      "ZAK: Concert bezoek in Doornroosje",
+	      "De ZAK gaat met de voetjes van de vloer tijdens een bezoek ".
+	      "aan Doornroosje op 16 maart, waar Heidevolk (onze Gelderse ".
+	      "folk grootheden), Norther en bovenal Turisas (Wie kent ze ".
+	      "niet: Battle Metal en Rasputin!) acte de presence zullen ".
+	      "geven. Of, zoals ze zelf zeggen: \"Roert den trom! Steekt den ".
+	      "bazuin! Scherpt uw zwaard en sluit vrouwen en dochters op! ".
+	      "Turisas is wedergekeerd.\" Kaartjes kosten 10 euro, het begint ".
+	      "om 19:00 uur. Laat even weten of je meegaat aan Jorg, dan gaan ".
+	      "we gezellig samen."),
+	array("Themaborrel",
+	      "19 Maart",
+	      "Woensdag 19 Maart",
+	      "Themaborrel",
+	      "De eerste thema borrel van het nieuwe barhoofd Anke<br/>".
+	      "Vanaf 20:30 in cafe Dollars"),
+	array("Playbackshow",
+	      "26 Maart",
+	      "Woensdag 26 Maart",
+	      "Extreme Playbackshow",
+	      "De beruchte Karpe Noktem Extreme Playbackshow. Zoals elk jaar ".
+	      "de ideale mogelijkheid om je te verkleden als je (jeugd)helden".
+	      " en je medeleden te vermaken. Een locatie volgt ASAP, maar geef".
+	      " je alvast op bij het bestuur of op het forum!")
 );
 
 ?>
