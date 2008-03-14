06d3f783f7e064aeefc01922fda08cea37b7f1fc
Bas Westerbaan <bas@fsfe.org>
2008-03-15 00:52:39 +0100

    Ragweek tekst iets uitgebreider.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/media.php
+++ actions/media.php
@@ -8,6 +8,9 @@
 	<li><a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem"
 	    >video op het voxlog</a> en</li>
 	<li>op <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142"
-	    >CampusTV</a> van het benefiet feest gegeven in de RAGweek van 2008.
+	    >CampusTV</a> van het benefiet feest gegeven samen met
+	    <a href="http://www.carolusmagnus.nl/">NSV Carolus Magnus</a>
+	    in de <a href="http://www.ru.nl/ragweek/">ragweek</a>
+	    van 2008.
 </ul>
 <?php default_footer(); ?>
