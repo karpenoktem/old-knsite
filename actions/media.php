c0fef2ea7c1cae4f6cefacbd0064087a596e9b09
Bas Westerbaan <bas@fsfe.org>
2008-03-15 00:48:21 +0100

    Toevoegen media pagina.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/media.php
@@ -0,0 +1,13 @@
+<?php set_title('Media');
+      default_header(); ?>
+<ul>
+	<li><a href="http://www.flickr.com/photos/12267979@N04/"
+	     >Fotos van Karpe Noktem</a> en</li>
+	<li><a href="http://www.flickr.com/photos/12264801@N04/"
+	     >Pluk het Nacht weekend 2007</a> op Flickr.</li>
+	<li><a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem"
+	    >video op het voxlog</a> en</li>
+	<li>op <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142"
+	    >CampusTV</a> van het benefiet feest gegeven in de RAGweek van 2008.
+</ul>
+<?php default_footer(); ?>
