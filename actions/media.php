b0daca88f36914c5a967e064a87e6f6220ec1837
Bas Westerbaan <bas@fsfe.org>
2008-03-15 01:15:35 +0100

    Nieuwe foto link en ietswat ordelijker maken van de media pagina.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/media.php
+++ actions/media.php
@@ -5,12 +5,14 @@
 	     >Fotos van Karpe Noktem</a> en</li>
 	<li><a href="http://www.flickr.com/photos/12264801@N04/"
 	     >Pluk het Nacht weekend 2007</a> op Flickr.</li>
-	<li><a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem"
-	    >video op het voxlog</a> en</li>
-	<li>op <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142"
-	    >CampusTV</a> van het benefiet feest gegeven samen met
+	<li><a href="http://www.carolusmagnus.nl/page_types/photogallery/show_collection.asp?collection_id=116"
+	     >Fotos op de site van Carolus</a>; videos 
+	    <a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem"
+	    >op het voxlog</a> en</li>
+	    <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142"
+	    >op CampusTV</a> van het <em>benefiet feest</em> gegeven samen met
 	    <a href="http://www.carolusmagnus.nl/">NSV Carolus Magnus</a>
 	    in de <a href="http://www.ru.nl/ragweek/">ragweek</a>
-	    van 2008.
+	    van 2008.</li>
 </ul>
 <?php default_footer(); ?>
