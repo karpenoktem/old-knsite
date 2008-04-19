eb029f5a0b2a6bb1d9ca538f47ac1452b6fc2abe
Bas Westerbaan <bas@fsfe.org>
2008-04-19 15:35:21 +0200

    Agenda updates naar de ledenmail van 15 April.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- config.agenda.php
+++ config.agenda.php
@@ -6,24 +6,6 @@ $cfg['agenda'] = array(
 	      "Datum op agenda pagina",
 	      "Titel op agenda pagina",
 	      "Tekst op agenda pagina"), */
-	array("Poezieborrel",
-	      "9 April",
-	      "Woensdag 9 april",
-	      "Poezieborrel",
-	      "Aangezien jullie een week eerder inspiratie hebben kunnen ".
-	      "opdoen, mogen jullie dit keer zelf. Schrijf je beste poezie ".
-	      "(proza mag ook hoor) en doe je medeleden versteld staan. ".
-	      "Inschrijven kan bij het bestuur, of op de barlijst.<br/>".
-	      "Zaaltje boven Dollars, vanaf 20:30 uur."),
-	array("Debatavond",
-	      "16 April",
-	      "Woensdag 16 april",
-	      "Debatavond",
-	      "In tegenstelling tot eerdere berichten geen beachborrel (don't ".
-	      "shoot the messenger...) maar een interne debatavond. Daan en ".
-	      "Jorne zullen een aantal pittige stellingen bedenken waarover ".
-	      "we met zijn allen van mening over kunnen verschillen.<br/>".
-	      "Cafe 't Haantje, vanaf 21:00"),
 	array("Karpe Rokt'em",
 	      "23 April",
 	      "Woensdag 23 April",
@@ -39,7 +21,24 @@ $cfg['agenda'] = array(
 	      "Woensdag 30 April",
 	      "Vodkaborrel",
 	      "Wat is er nou leuker dan op konninginedag oranje wodka drinken ".
-	      "in de Bijstand? Niets toch? We verzamelen om 20:30 uur.")
+	      "in de Bijstand? Niets toch? We verzamelen om 20:30 uur."),
+	array("Themaborrel",
+	      "7 Mei",
+	      "Woensdag 7 Mei",
+	      "Themaborrel",
+	      "Tijd voor een onvervalste themaborrel. Mocht je nog iets leuks ".
+	      "weten kun je dit natuurlijk altijd met onze lieftallige ".
+	      "barvrouw Anke bespreken.<br/>".
+	      "Vanaf 20:30 in Cafe Dollars"),
+	array("Pokeravond",
+	      "14 Mei",
+	      "Woensdag 14 Mei",
+	      "Pokeravond",
+	      "Wegens succes geprolongeerd, de pokeravond in cafe 't Haantje. ".
+	      "Vooraf inschrijven is vereist, en dit kan bij Daan ".
+	      "<". email('hertogdaan@gmail.com')."> of op de barlijst. ".
+	      "We beginnen om 21:00 uur stipt.  Wees op tijd anders kun je ".
+	      "niet meer meedoen.")
 );
 
 ?>
