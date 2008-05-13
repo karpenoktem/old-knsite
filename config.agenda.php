405b4119a2bb6cc58476b393491a791e72e1bddf
Bas Westerbaan <bas@fsfe.org>
2008-05-13 18:12:59 +0200

    Agenda updates naar de ledenmail van 13 mei.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- config.agenda.php
+++ config.agenda.php
@@ -29,7 +29,37 @@ $cfg['agenda'] = array(
 	      "<i>Boondock Saints</i> op het programma. Suggesties zijn welkom".
 	      " en op dit adres te melden.  Aangezien Daan jammer genoeg geen".
 	      " filantropische instelling is, is dit een zogeheten \"bring".
-	      " your own beer\" feestje.")
+	      " your own beer\" feestje."),
+	array("Zusterweekend",
+	      "6-8 juni",
+	      "Weekend van 6, 7 en 8 juni",
+	      "Zusterweekend",
+	      "Samen met onze zustervereniging uit Amsterdam, Cyclades, ".
+	      "organiseren wij dit jaar het traditionele zusjesweekend. ".
+	      "Inschrijven kan bij Judith ".email('ik_moi@hotmail.com').
+	      "), over het programma en de kosten geef ik zo snel mogelijk ".
+	      "uitsluitsel."),
+	array("ALV",
+	      "11 juni",
+	      "Woensdag 11 juni",
+	      "Laatste ALV",
+	      "De laatste ALV van dit jaar. Het belangrijkste punt is de ".
+	      "benoeming van een nieuw bestuur. Het huidige bestuur zal ".
+	      "een voordracht doen. We beginnen, net als de vorige keer, ".
+	      "om 21:00 uur stipt in de zaal boven cafe Dollars."),
+	array("Eindfeest",
+	      "18 juni",
+	      "Woensdag 18 juni",
+	      "Eindfeest",
+	      "Zoals elk jaar sluiten we het jaar officieel af met een ".
+	      "gemaskerd bal. Laura, Niels en Daan hebben een prachtige ".
+	      "deal met Vivaldi's (aan de Waalkade) gesloten. Voor 21 euro ".
+	      "(22,50 voor externen) kunnen we tussen 22:00 uur en 3:00 ".
+	      "uur onbeperkt genieten van bier, wijn en fris en zullen onze ".
+	      "huisDj's er een geweldig feest van maken. Het bestuur zal ".
+	      "vanaf morgen kaartjes verkopen, dus neem voldoende geld mee!".
+	      " Voor degene die het best in stijl verschijnt is er ".
+	      "een beloning.")
 );
 
 ?>
