fd48ebb3705ac66504be606ef6e5255a06ee0b8b
Bas Westerbaan <bas@fsfe.org>
2008-08-15 17:13:53 +0200

    contact: duidelijker en nieuw e-mail adres webmaster.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/contact.php
+++ actions/contact.php
@@ -3,6 +3,6 @@
 <p>Heeft u een vraag? Kunnen wij u helpen? De voorzitter houdt iedere dag
 spreekuur - alleen op afspraak, tenminste 1 dag van tevoren gemeld via
 <?php echo email('voorzitter@karpenoktem.nl'); ?> - tijdens de pauze.</p>
-<p>Voor vragen ofwel opmerkingen over karpenoktem.nl kunt u mailen met
-<?php echo email('bas@fsfe.org'); ?>.</p>
+<p>Voor vragen ofwel opmerkingen over de website karpenoktem.nl kunt u 
+mailen met <?php echo email('webmaster@karpenoktem.nl'); ?>.</p>
 <?php default_footer(); ?>
