a7a5680ab33d72d5e658900ad3bffd5045bd6687
Bas Westerbaan <bas@fsfe.org>
2008-08-16 16:58:48 +0200

    lidworden: Broken link.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lidworden.php
+++ actions/lidworden.php
@@ -1,6 +1,6 @@
 <?php set_title('Lid worden');
 	  default_header(); ?>
 <p>Kom eens een keer langs bij een van onze 
-<a href="<?php echo curi('agenda'); ?>">activiteiten</a> of stuur een e-mailtje
+<a href="<?php echo auri('agenda'); ?>">activiteiten</a> of stuur een e-mailtje
 naar <?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
 <?php default_footer(); ?>
