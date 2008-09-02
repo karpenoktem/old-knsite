98fa50953cb29110833874b41e634e1a4107baca
Bas Westerbaan <bas@fsfe.org>
2008-09-03 01:07:24 +0200

    merchandise: add tinus' e-mail for thsirts
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/merchandise.php
+++ actions/merchandise.php
@@ -1,5 +1,6 @@
 <?php set_title('Merchandise');
 	  default_header(); ?>
 <p>Voor zo een kek Karpe Noktem <strong>t-shirt</strong> kun je bij Tinus wezen
-tijdens een normale <a href="<?php echo auri('agenda') ?>">borrel</a>.</p>
+tijdens een normale <a href="<?php echo auri('agenda') ?>">borrel</a>. E-Mail
+<?php echo email('tinus@karpenoktem.nl'); ?> vantevoren om het te regelen.</p>
 <?php default_footer(); ?>
