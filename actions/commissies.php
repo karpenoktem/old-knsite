e2b6ef22fd9f0e84e2b12bb5e70b4b3b90147104
Bas Westerbaan <bas@fsfe.org>
2008-09-08 00:33:46 +0200

    bug: forgot the commissies page
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/commissies.php
@@ -0,0 +1,30 @@
+<?php set_title('Commissies');
+	  default_header(); ?>
+<dl>
+	<dt>Akta<dt>
+	<dd>De Akta commissie schrijft het ledenblad <em>Akta Nokturna</em>.
+	Heb je een quote, column of een andere tekst voor in de Akta,
+	e-mail dan <?php echo email('akta@karpenoktem.nl'); ?>.</dd>
+	<dt>Barco</dt>
+	<dd>De Bar commissie zorgt voor versgetapt bier op de borrels.
+	Wil je de barco versterken, e-mail dan naar
+	<?php echo email('barco@karpenoktem.nl'); ?>.</dd>
+	<dt>FotoCie</dt>
+	<dd>De Foto commissie maakt foto's van activiteiten.  Heb jij fotos
+	die je op de site moeten komen, e-mail dan naar
+	<?php echo email('fotocie@karpenoktem.nl'); ?>.</dd>
+	<dt>ProCo</dt>
+	<dd>De Promotie Commissie zorgt ervoor dat we blijven groeien. Heb
+	jij een goed idee voor promotie, of wil je ons helpen:
+	<?php echo email('proco@karpenoktem.nl'); ?>.</dd>
+	<dt>Webcie</dt>
+	<dd>De Web commissie is verantwoordelijk voor de website en alles
+	wat daar bij komt kijken.  De webcie zit achter
+	<?php echo email('webcie@karpenoktem.nl'); ?>.</dd>
+	<dt>WeekendCie</dt>
+	<dd>De Weekend commissie organiseert de spetterende verenigings
+	weekenden.  Wil je mee helpen, je inschrijven voor een weekend
+	of heb je iets te vragen, e-mail dan naar
+	<?php echo email('weekendcie@karpenoktem.nl'); ?>.</dd>
+</dl>
+<?php default_footer(); ?>
