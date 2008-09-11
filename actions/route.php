62c4d4588004faeb0e02eeb566b9cd12d3422d6d
Bas Westerbaan <bas@fsfe.org>
2008-09-11 12:50:49 +0000

    route: Voeg een route pagina toe
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/route.php
@@ -0,0 +1,13 @@
+<?php set_title('Route');
+	  default_header(); ?>
+<p>Op de maandagen zitten we in de kelder van de Villa. Deze ligt links aan de 
+directe weg van het Centraal Station naar het Keizer Karel plein.</p>
+<address>Villa van Schaeck<br/>
+Van Schaeck Mathonsingel 10<br/>
+Nijmegen</address>
+<p>
+<iframe width="425" height="350" frameborder="0" scrolling="no" 
+        marginheight="0" marginwidth="0" 
+	src="http://maps.google.com/?ie=UTF8&amp;t=h&amp;s=AARTsJqzARj-Z8VnW5pkPMLMmZbqrJcYpw&amp;ll=51.842718,5.85641&amp;spn=0.00232,0.00456&amp;z=17&amp;output=embed"></iframe><br />
+<small><a href="http://maps.google.com/?ie=UTF8&amp;t=h&amp;ll=51.842718,5.85641&amp;spn=0.00232,0.00456&amp;z=17&amp;source=embed" style="text-align:left">Grotere map</a></small>
+<?php default_footer(); ?>
