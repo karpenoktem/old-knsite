be7e0730f52b1dd431006cd6dd55a9f4aa2b38b4
Bas Westerbaan <bas@fsfe.org>
2008-09-11 12:26:00 +0000

    merchandise: verwijs naar spul@.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/merchandise.php
+++ actions/merchandise.php
@@ -1,6 +1,7 @@
 <?php set_title('Merchandise');
 	  default_header(); ?>
-<p>Voor zo een kek Karpe Noktem <strong>t-shirt</strong> kun je bij Tinus wezen
-tijdens een normale <a href="<?php echo auri('agenda') ?>">borrel</a>. E-Mail
-<?php echo email('tinus@karpenoktem.nl'); ?> vantevoren om het te regelen.</p>
+<p>Wil je zo een kek Karpe Noktem <strong>t-shirt</strong>; ben je
+geinteresseerd in een <strong>vlag</strong> of heb jij een goed idee voor
+ander Karpe Noktem spul?  E-Mail dan naar
+<?php echo email('spul@karpenoktem.nl'); ?>.</p>
 <?php default_footer(); ?>
