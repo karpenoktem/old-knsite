4f1ade39bd5cba3404058f4fd6ad47f06fe54599
Bas Westerbaan <bas@fsfe.org>
2008-09-11 15:32:02 +0000

    activiteiten: add reference to the maandag borrel
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/activiteiten.php
+++ actions/activiteiten.php
@@ -7,6 +7,8 @@ activiteiten. Een kleine greep uit voorgaande hoogtepunten: Karpe Rockt'em, De
 nacht van de poëzie, Het jaarlijkse gemaskerd bal &amp; Karpe Noktem weekend.
 Ook verzeil je regelmatig in een vreemde stad tijdens onze Roadtrips en feesten
 bij onze zusterverenigingen van het ZEUS-verband.</p>
-<p>Wil je concrete up-to-date informatie over ons programma? Kijk even in
-onze <a href="<?php echo auri('agenda') ?>">agenda</a></p>
+<p>Natuurlijk is er elke maandag een gezellige borrel in onze vaste stek, de
+<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a>.  Wil je concrete 
+up-to-date informatie over ons programma? Kijk even in
+onze <a href="<?php echo auri('agenda') ?>">agenda</a>.</p>
 <?php default_footer(); ?>
