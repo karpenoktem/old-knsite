a550e1b5060267b5450d642c082291c1f3f75974
Bas Westerbaan <bas@fsfe.org>
2008-09-11 15:29:21 +0000

    lidworden: Add "Maandagavond borrels" as example
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lidworden.php
+++ actions/lidworden.php
@@ -1,6 +1,8 @@
 <?php set_title('Lid worden');
 	  default_header(); ?>
 <p>Kom eens een keer langs bij een van onze 
-<a href="<?php echo auri('agenda'); ?>">activiteiten</a> of stuur een e-mailtje
-naar <?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
+<a href="<?php echo auri('agenda'); ?>">activiteiten</a>, bijvoorbeeld de maandagavond
+borrels in <a href="<?php echo auri('route'); ?>">de Villa van Schaeck</a>, of stuur 
+een e-mailtje naar 
+<?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
 <?php default_footer(); ?>
