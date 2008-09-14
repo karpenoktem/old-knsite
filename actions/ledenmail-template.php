40ac70e4bec3a3de63ed2a01365eda8cee9943b2
Bas Westerbaan <bas@fsfe.org>
2008-09-14 19:36:04 +0200

    ledenmail-template: a template for the ledenmail
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/ledenmail-template.php
@@ -0,0 +1,12 @@
+<?php set_title('Agenda');
+	  require_once('lib/agenda.php'); ?>
+<html>
+	<head></head>
+	<body>
+	<?php foreach($cfg['agenda'] as $item) { ?>
+	<p><strong><?php echo $item[3]; ?></strong><br/>
+	<em><?php echo $item[2]; ?></em><br/>
+	<?php echo $item[4]; ?>
+	<?php } ?>
+	</body>
+</html>
