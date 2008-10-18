8bdce1f9b264272c81a3197066090b2cf5c2415b
Bas Westerbaan <bas@fsfe.org>
2008-10-18 16:22:05 +0200

    agenda: google calendar en interested form on top
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/agenda.php
+++ actions/agenda.php
@@ -2,6 +2,10 @@
 	  include_stylesheet('agenda');
 	  require_once('lib/agenda.php');
 	  default_header(); ?>
+<p><a href="http://www.google.com/calendar/embed?src=vssp95jliss0lpr768ec9spbd8%40group.calendar.google.com&ctz=Europe/Paris">Op Google Calendar</a>.
+Geen lid en op de hoogte blijven van onze activiteiten?
+Zet jezelf op de geinteresseerden e-mail lijst:</p>
+<p><?php echo interested_form(); ?></p>
 <ul id="fullagenda">
 	<?php foreach($cfg['agenda'] as $item) { ?>
 	<li><a name="<?php echo $item['key']; ?>"></a>
@@ -10,10 +14,4 @@
 	<div class="desc"><?php echo $item[4]; ?></div></li>
 	<?php } ?>
 </ul>
-
-<p>We houden ook <a href="http://www.google.com/calendar/embed?src=vssp95jliss0lpr768ec9spbd8%40group.calendar.google.com&ctz=Europe/Paris">een Google Calendar</a> bij. Leden krijgen elke week
-een ledenmail met de agenda er in.  Ben je geen lid, maar wel geinteresseerd
-in onze activiteiten, dan kun je jezelf op de geinteresseerden e-mail lijst
-zetten:</p>
-<p><?php echo interested_form(); ?></p>
 <?php default_footer(); ?>
