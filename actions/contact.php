86d211be75a315c6596507f196608ac29e5650cb
Bas Westerbaan <bas@fsfe.org>
2008-10-18 16:33:10 +0200

    Move contact info from bestuur to contact
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/contact.php
+++ actions/contact.php
@@ -3,6 +3,16 @@
 <p>Heeft u een vraag? Kunnen wij u helpen? De voorzitter houdt iedere dag
 spreekuur - alleen op afspraak, tenminste 1 dag van tevoren gemeld via
 <?php echo email('voorzitter@karpenoktem.nl'); ?> - tijdens de pauze.</p>
+<p>Voor verzoeken, aanbiedingen en andere dingen van algemene zaken kunt u
+zich wenden tot Secretaris: 
+<?php echo email('secretaris@karpenoktem.nl'); ?></p>
+<p>Voor alle financiele zaken, mag u zich wenden tot de Penningmeester:
+ <?php echo email('penningmeester@karpenoktem.nl'); ?>.</p>
 <p>Voor vragen ofwel opmerkingen over de website karpenoktem.nl kunt u 
 mailen met <?php echo email('webcie@karpenoktem.nl'); ?>.</p>
+<p>Kerstkaarten, uitnodigingen, rekeningen en alle andere zaken kunt
+u sturen naar:</p>
+<blockquote>ASV Karpe Noktem<br/>
+Van Schaeck Mathonsingel 10<br/>
+6512 AP Nijmegen</blockquote>
 <?php default_footer(); ?>
