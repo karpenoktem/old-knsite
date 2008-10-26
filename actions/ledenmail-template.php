429e79f55fb59421cf6221f765d69eea67b851e4
Bas Westerbaan <bas@fsfe.org>
2008-10-26 14:40:20 +0000

    ledenmail-template: voeg overzicht toe
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/ledenmail-template.php
+++ actions/ledenmail-template.php
@@ -3,6 +3,11 @@
 <html>
 	<head></head>
 	<body>
+	<ul>
+	<?php foreach($cfg['agenda'] as $item) { ?>
+	<li><em><?php echo $item[2]; ?></em>: <?php echo $item[3]; ?></li>
+	<?php } ?>
+	</ul>
 	<?php foreach($cfg['agenda'] as $item) { ?>
 	<p><strong><?php echo $item[3]; ?></strong><br/>
 	<em><?php echo $item[2]; ?></em><br/>
