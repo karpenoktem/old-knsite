c4b46d284558f9405d57575d0c53b38086ce47f4
Bas Westerbaan <bas@fsfe.org>
2008-11-26 20:27:41 +0000

    Zusjes: lexographically sorted & added SAAM
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/zusjes.php
+++ actions/zusjes.php
@@ -7,13 +7,14 @@ zusterverbnd ZEUS (dit staat voor Zusterlijke Eenheid Uit Saamhorigheid),
 dat bestaat uit zeven leden; de verenigingen
 </p>
 <ul>
+<li><a href="http://www.biton.nl/">B.I.T.O.N.</a> uit Utrecht,</li>
+<li><a href="http://www.vslcatena.nl/">Catena</a> uit Leiden,</li>
 <li><a href="http://www.cleopatra-groningen.nl/">Cleopatra</a>
 	uit Groningen,</li>
-<li><a href="http://www.wolweze.nl/">Wolweze</a> uit Leeuwarden,</li>
 <li><a href="http://www.sv-cyclades.nl/">Cyclades</a> uit Amsterdam,</li>
-<li><a href="http://www.vslcatena.nl/">Catena</a> uit Leiden,</li>
-<li><a href="http://www.biton.nl/">B.I.T.O.N.</a> uit Utrecht en</li>
-<li><a href="http://www.saam.nl/">Wolbodo</a> uit Delft.</li>
+<li><a href="http://www.saam.nl/">SAAM</a> uit Deventer,</li>
+<li><a href="http://www.wolbodo.nl/">Wolbodo</a> uit Delft en</li>
+<li><a href="http://www.wolweze.nl/">Wolweze</a> uit Leeuwarden.</li>
 </ul>
 <p>Samen met deze verenigingen organiseren wij feesten, weekenden en hebben wij
 andere samenwerkingsverbanden.</p>
