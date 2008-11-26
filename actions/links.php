e77f1a53eee80ca47a75b355c19425df009471c6
Bas Westerbaan <bas@fsfe.org>
2008-11-26 20:39:23 +0000

    Various updates to the links page.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/links.php
+++ actions/links.php
@@ -3,8 +3,18 @@
 	<p>Karpe Noktem op</p>
 	<ul>
 		<li><a href="http://karpenoktem.hyves.nl/">Hyves</a>,</li>
-		<li><a href="http://www.youtube.com/KarpeNoktem">YouTube</a> (video's),</li>
-		<li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr (foto's)</a>,</li>
-		<li>en <a href="http://nl.wikipedia.org/wiki/Karpe_Noktem">Wikipedia</a>.</li>
+		<li><a href="http://www.youtube.com/KarpeNoktem"
+		>YouTube</a> (video's),</li>
+		<li><a href="http://www.flickr.com/photos/12267979@N04/"
+		>Flickr</a> (oude foto's: <a href="http://karpenoktem.nl/fotos"
+		>hier</a> staan de nieuwe),</li>
+		<li>en <a href="http://nl.wikipedia.org/wiki/Karpe_Noktem"
+		>Wikipedia</a>.</li>
+	</ul>
+	<p>Maar vergeet niet</p>
+	<ul>
+		<li><a href="<?php echo auri('zusjes') ?>">Onze zusjes</a>,</li>
+		<li><a href="<?php echo auri('forum') ?>">Forum</a> en</li>
+		<li>onze <a href="http://karpenoktem.nl/wiki">Wiki</a>.</li>
 	</ul>
 <?php default_footer(); ?>
