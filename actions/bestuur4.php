8f4b801b4da18d113ed62089985e0be7b2752eb5
Bas Westerbaan <bas@fsfe.org>
2008-12-10 22:06:29 +0100

    Rename bestuur to bestuur4.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/bestuur4.php
@@ -0,0 +1,14 @@
+<?php set_title('Vierde Bestuur');
+	  default_header(); ?>
+<img class="big" src="<?php echo curi('img/bestuur4.jpg'); ?>" />
+<p><strong>Het vierde bestuur van Karpe Noktem</strong> zit vol met echte 
+coinnaisseurs van het
+vak. Bram Balk (Voorzitter) heeft zijn sporen verdiend als hoofdredacteur van
+het Algemeen Nijmeegs Studentenblad. Daan Fifis du Pont (Vice-voorzitter) kent
+het klappen van de zweep als ex-bestuurslid van debatvereniging Trivium. Jorg
+Kennis (secretaris) is veteraan in het bestuur en zorgt voor continuiteit en
+ervaring. Jorne van de Veen (Penningmeester) geldt als expert op het gebied
+van Financien. De grote creatieve geest in het bestuur is Judith Brinkkemper,
+die als oprichtster van de LoCo verse en originele ideeen de vrije ruimte
+geeft.</p>
+<?php default_footer(); ?>
