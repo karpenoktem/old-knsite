82fd843a4ca2b62c619046ffba63bb4e2c4dc16e
Bas Westerbaan <bas@fsfe.org>
2008-12-25 12:58:09 +0000

    zusjes: SAAM is opgeheven
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/zusjes.php
+++ actions/zusjes.php
@@ -12,7 +12,6 @@ dat bestaat uit zeven leden; de verenigingen
 <li><a href="http://www.cleopatra-groningen.nl/">Cleopatra</a>
 	uit Groningen,</li>
 <li><a href="http://www.sv-cyclades.nl/">Cyclades</a> uit Amsterdam,</li>
-<li><a href="http://www.saam.nl/">SAAM</a> uit Deventer,</li>
 <li><a href="http://www.wolbodo.nl/">Wolbodo</a> uit Delft en</li>
 <li><a href="http://www.wolweze.nl/">Wolw&ecirc;ze</a> uit Leeuwarden.</li>
 </ul>
