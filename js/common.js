a4510376b883cafd9cebc3051c2e55033a61e0ce
Bas Westerbaan <bas@fsfe.org>
2009-01-29 15:28:38 +0000

    js/common: lustrumCountdown: better output for edge cases
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- js/common.js
+++ js/common.js
@@ -7,23 +7,49 @@ function common_init() {
 	lustrum_countDown_tick()
 }
 
+function humane_enum(a) {
+	var ret = "";
+	var first = true;
+	for(var i=0; i < a.length; i++) {
+		if(first) first = false;
+		else ret += i == a.length - 1 ? ' en ' : ', ';
+		ret += a[i];
+	}
+	return ret;
+}
+
 function lustrum_countDown_tick() {
-	rms = (new Date(2009, 5, 25) - new Date());
-	dagen = parseInt(rms / 1000 / 60 / 60 / 24);
-	uren = parseInt(rms / 1000 / 60 / 60 - dagen * 24);
-	minuten = parseInt(rms / 1000 / 60 - (dagen * 24 + uren) * 60);
-	seconden = parseInt(rms / 1000 - ((dagen * 24 + uren) * 60 + minuten) * 60);
-	dagens =  dagen != 1 ? "dagen" : "dag";
-	urens = uren != 1 ? "uren" : "uur";
-	minutens = minuten != 1 ? "minuten" : "minuut";
-	secondens = seconden != 1 ? "seconden" : "seconde";
+	var rms = (new Date(2009, 5, 25) - new Date());
+	var neg = rms < 0;
+	rms = neg ? -rms : rms;
+	
+	var dagen = parseInt(rms / 1000 / 60 / 60 / 24);
+	var uren = parseInt(rms / 1000 / 60 / 60 - dagen * 24);
+	var minuten = parseInt(rms / 1000 / 60 - (dagen * 24 + uren) * 60);
+	var seconden = parseInt(rms / 1000 - ((dagen * 24 + uren) * 60 + minuten) * 60);
+	var dagens =  dagen != 1 ? "dagen" : "dag";
+	var urens = uren != 1 ? "uren" : "uur";
+	var minutens = minuten != 1 ? "minuten" : "minuut";
+	var secondens = seconden != 1 ? "seconden" : "seconde";
+		
+	var v = [dagen.toString() + " " + dagens,
+	         uren.toString() + " " + urens,
+	         minuten.toString() + " " + minutens,
+	         seconden.toString() + " " + secondens];
+
+	if(dagen == 0) {
+		if(uren == 0) {
+			if(minuten == 0)
+				v = v.slice(3);
+			else
+				v = v.slice(2);
+		} else
+			v = v.slice(1);
+	}
 
 	objById('lustrumCountDown').innerHTML =
-		dagen.toString() + " "+dagens+", " +
-		uren.toString() + " "+urens+", " +
-		minuten.toString() + " "+minutens+" en " +
-		seconden.toString() + " "+secondens+
-		" tot het Lustrum!";
+		humane_enum(v) +
+		(neg ? " in het Lustrum!" : " tot het Lustrum!");
 	setTimeout('lustrum_countDown_tick()', 1000)
 }
 
