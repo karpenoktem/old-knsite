f39600b2838320aa5309b452e5482a758a1d230c
Bas Westerbaan <bas@fsfe.org>
2009-04-22 18:32:58 +0000

    lustrum: Marlies haar revisie
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lustrum.php
+++ actions/lustrum.php
@@ -2,65 +2,63 @@
 	  include_stylesheet('lustrum');
 	  default_header(); ?>
 <dl id="lustrumAgenda">
-<dt>Maandagavond &mdash; Borrel</dt>
+<dt>Maandagavond &mdash; borrel</dt>
 <dd>
-Een maandagavondborrel als alle anderen, alleen dit keer zal er om 22:00 een
-presentatie zijn van enkele oud&mdash;bestuursleden, die wat zullen vertellen over 
-de vroege geschiedenis van karpe Noktem. Sprekers zullen zijn Giedo Jansen,
-Alette Ouwens en Bram Balk. Wellicht zullen enkele externen ook aanwezig zijn.
-Uiteraard gelden voor deze borrel de gebruikelijk prijzen en aanbod van ASV
-Karpe Noktem.
+Een maandagavondborrel als alle andere &mdash; alleen zullen deze keer
+oud-bestuursleden om 22:00 een presentatie geven!  De ex-voorzitters
+Giedo Jansen, Alette Ouwens en Bram Balk vertellen over de vroegste tijden
+uit de geschiedenis van Karpe Noktem.  Wellicht zullen er ook enkele externen
+aanwezig zijn.  Uiteraard geldt op deze borrel het gebruikelijke bartarief en
+-aanbod van ASV Karpe Noktem.
 </dd>
 
-<dt>Dinsdagavond &mdash; Eten met Leden</dt>
+<dt>Dinsdagavond &mdash; eten met Leden</dt>
 <dd>
-In het gebouw van Scouting Trajanus in Nijmegen zal onze allereerste offici&euml;le
-Eten Met Leden activiteit plaatsvinden, waarbij de LustrumCie zal zorgen voor
-het nodige drinken en enkele versnaperingen, en jullie als leden allemaal een
-maaltijd meenemen, om uit te wisselen met je medeleden, die ook allemaal iets
-hebben meegenomen.
-Dus, maak een frisse salade, een vullend hoofdgerecht, heerlijke soep of een
-tongstrelend toetje, en voer je medeleden (dan wel niet op romantische wijze).
-Er zullen opwarmmogelijkheden zijn, en ruim tijd om uit te buiken en na te
-genieten.
+In het gebouw van Scouting Traianus Nijmegen zal onze allereerste offici&euml;le
+'eten met leden'-activiteit plaatsvinden.  Tijdens deze megapicknick neemt ieder
+lid een maaltijd mee om te delen met zijn of haar medeleden, die op hun beurt ook
+allemaal iets te eten hebben meegenomen.  Ook de LustrumCie zorgt voor het nodige
+drinken en enkele versnaperingen.  Dus, maak een frisse salade, een vullend
+hoofdgerecht, heerlijke soep of een tongstrelend toetje en voer daarmee je
+medeleden (dan wel niet op romantische wijze)!  Er is overigens een mogelijkheid
+tot het opwarmen van je gerecht.  Na het voedselfestijn is er ruim tijd om uit te
+buiken en na te genieten!
 </dd>
  
-<dt>Woensdagmiddag &mdash; Spelen en Relaxen in park Brakkestein</dt>
+<dt>Woensdagmiddag &mdash; spelen en relaxen in park Brakkenstein</dt>
 <dd>
-na de zware maaltijd van dinsdagavond moeten al die extra calori&euml;n er
-natuurlijk wel weer vanaf &mdash; of niet. Dus daarom gaan we naar park brakkestein,
-om daar in de middagzon te genieten van enkele sportieve activiteiten &mdash; of
-niet. Als je daar namelijk geen zin in hebt, dan ga je uiteraard een beetje
-chillen in het gras. iedereen is welkom om wat picknick&mdash;benodigdheden mee te
-nemen, zoals sandwiches, pannekoeken, picknickmanden, tafelkleden, limonade,
-en meer van die dingen. De LustrumCie zal zorgen voor bier, fris en wijn.
+Na de zware maaltijd van dinsdagavond moeten al die extra calorie&euml;n er
+natuurlijk wel weer vanaf (of niet).  Daarom gaan we 's woensdags naar Park
+Brakkenstein om daar in de middagzon te genieten van enkele sportieve activiteiten
+(of niet).  Als je namelijk geen zin in hebt in al dat geren en gespring kun je
+uiteraard ook een beetje chillen in het gras.  Iedereen is welkom om wat
+picknick-benodigdheden mee te nemen, zoals sandwiches, pannekoeken, picknickmanden,
+tafelkleden, limonade, en zo voorts.  De LustrumCie zal zorgen voor bier, fris
+en wijn.
 </dd>
 
-<dt>Donderdagavond &mdash; Het Eerste Lustrumfeest in de Villa</dt>
+<dt>Donderdagavond &mdash; het eerste lustrumfeest in de Villa</dt>
 <dd>
 <img src="<?php echo curi('img/myrkvar.jpg'); ?>" class="right" />
-Het hoogtepunt van de week! Tijdens deze explosieve avond in de grote zaal
-van de Villa is iedereen welkom om mee te vieren met het lustrum van Karpe
-Noktem! De avond zal stevig maar vrolijk geopend worden door Folk Metal band
-<a href="http://www.myrkvar.nl/">Myrkvar</a>
-, en vervolgens zullen de springerige Folk Punk riffjes van
+Het hoogtepunt van de week!  Tijdens deze explosieve avond in de grote zaal van
+de Villa is iedereen welkom om het eerste lustrum van Karpe Noktem mee te vieren!
+De avond zal stevig, maar vrolijk geopend worden door folkmetalband
+<a href="http://www.myrkvar.nl/">Myrkvar</a>;
+vervolgens zullen de springerige folkpunkriffjes van
 <a href="http://www.baggerunited.nl/">Bagger United</a>
-je voeten verder de nacht in dragen. De avond wordt afgesloten door
-onze eigen DJ's Felix en Stonerhead.
-Ook zal dit Lustrumfeest tevens ons Zusjesfeest zijn, dus we kunnen bezoek
-verwachten uit alle hoeken en gaten van Nederland, die voor het eerst onze
-nieuwe lokatie te zien zullen krijgen!
-Gedurende de avond zullen consumpties 1 euro bedragen, zoals jullie gewend
-zijn van Karpe Noktem.
+je voeten verder de nacht in dragen!  De avond wordt voortgezet met onze eigen
+DJ's Felix en Stonehead.  Het lustrumfeest is tevens zusjesfeest, dus we
+kunnen bezoek verwachten uit alle hoeken en gaten van Nederland.  Consumpties
+zullen 1 euro bedragen, zoals jullie van Karpe Noktem gewend zijn.
 </dd>
  
-<dt>Vrijdagavond &mdash; Filmavond</dt>
+<dt>Vrijdagavond &mdash; filmavond</dt>
 <dd>
-Na de vermoeiende donderdagavond is het tijd voor rest en recreatie. Daarom
-zullen we vrijdagavond verzamelen in 't Haantje om daar, rustig en relaxed, te
-genieten van een film, die jullie zelf mogen kiezen via deze poll.
-Alle zusjes die nog in Nijmegen zijn gestrand, zijn natuurlijk van harte welkom
-om samen met Karpe noktem even uit te puffen.
+Na een vermoeiende donderdagavond is het tijd voor rust en recreatie.  Daarom
+zullen we ons vrijdagavond verzamelen in cafe 't Haantje om daar relaxed te
+genieten van een film die julli zelf mogen kiezen via de poll op onze site!  Alle
+zusjes die nog in Nijmegen zijn gestrand zijn natuurlijk van harte welkom om samen
+met Karpe Noktem uit te puffen.
 </dd>
 </dl>
 <?php default_footer(); ?>
