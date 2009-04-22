5c5b5f3724eac6e46800f3c0ff72afc3835d2000
Bas Westerbaan <bas@fsfe.org>
2009-04-22 13:29:59 +0000

    lustrum: voeg agenda van het lustrum toe MET PLAATJES!
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lustrum.php
+++ actions/lustrum.php
@@ -1,5 +1,66 @@
-<?php set_title('Lustrum');
+<?php set_title('Agenda van het eerste Lustrum');
+	  include_stylesheet('lustrum');
 	  default_header(); ?>
-<p>Hier komt een hele mooie aankondiging van het Lustrum.  Binnenkort.  Echt!
-</p>
+<dl id="lustrumAgenda">
+<dt>Maandagavond &mdash; Borrel</dt>
+<dd>
+Een maandagavondborrel als alle anderen, alleen dit keer zal er om 22:00 een
+presentatie zijn van enkele oud&mdash;bestuursleden, die wat zullen vertellen over 
+de vroege geschiedenis van karpe Noktem. Sprekers zullen zijn Giedo Jansen,
+Alette Ouwens en Bram Balk. Wellicht zullen enkele externen ook aanwezig zijn.
+Uiteraard gelden voor deze borrel de gebruikelijk prijzen en aanbod van ASV
+Karpe Noktem.
+</dd>
+
+<dt>Dinsdagavond &mdash; Eten met Leden</dt>
+<dd>
+In het gebouw van Scouting Trajanus in Nijmegen zal onze allereerste offici&euml;le
+Eten Met Leden activiteit plaatsvinden, waarbij de LustrumCie zal zorgen voor
+het nodige drinken en enkele versnaperingen, en jullie als leden allemaal een
+maaltijd meenemen, om uit te wisselen met je medeleden, die ook allemaal iets
+hebben meegenomen.
+Dus, maak een frisse salade, een vullend hoofdgerecht, heerlijke soep of een
+tongstrelend toetje, en voer je medeleden (dan wel niet op romantische wijze).
+Er zullen opwarmmogelijkheden zijn, en ruim tijd om uit te buiken en na te
+genieten.
+</dd>
+ 
+<dt>Woensdagmiddag &mdash; Spelen en Relaxen in park Brakkestein</dt>
+<dd>
+na de zware maaltijd van dinsdagavond moeten al die extra calori&euml;n er
+natuurlijk wel weer vanaf &mdash; of niet. Dus daarom gaan we naar park brakkestein,
+om daar in de middagzon te genieten van enkele sportieve activiteiten &mdash; of
+niet. Als je daar namelijk geen zin in hebt, dan ga je uiteraard een beetje
+chillen in het gras. iedereen is welkom om wat picknick&mdash;benodigdheden mee te
+nemen, zoals sandwiches, pannekoeken, picknickmanden, tafelkleden, limonade,
+en meer van die dingen. De LustrumCie zal zorgen voor bier, fris en wijn.
+</dd>
+
+<dt>Donderdagavond &mdash; Het Eerste Lustrumfeest in de Villa</dt>
+<dd>
+<img src="<?php echo curi('img/myrkvar.jpg'); ?>" class="right" />
+Het hoogtepunt van de week! Tijdens deze explosieve avond in de grote zaal
+van de Villa is iedereen welkom om mee te vieren met het lustrum van Karpe
+Noktem! De avond zal stevig maar vrolijk geopend worden door Folk Metal band
+<a href="http://www.myrkvar.nl/">Myrkvar</a>
+, en vervolgens zullen de springerige Folk Punk riffjes van
+<a href="http://www.baggerunited.nl/">Bagger United</a>
+je voeten verder de nacht in dragen. De avond wordt afgesloten door
+onze eigen DJ's Felix en Stonerhead.
+Ook zal dit Lustrumfeest tevens ons Zusjesfeest zijn, dus we kunnen bezoek
+verwachten uit alle hoeken en gaten van Nederland, die voor het eerst onze
+nieuwe lokatie te zien zullen krijgen!
+Gedurende de avond zullen consumpties 1 euro bedragen, zoals jullie gewend
+zijn van Karpe Noktem.
+</dd>
+ 
+<dt>Vrijdagavond &mdash; Filmavond</dt>
+<dd>
+Na de vermoeiende donderdagavond is het tijd voor rest en recreatie. Daarom
+zullen we vrijdagavond verzamelen in 't Haantje om daar, rustig en relaxed, te
+genieten van een film, die jullie zelf mogen kiezen via deze poll.
+Alle zusjes die nog in Nijmegen zijn gestrand, zijn natuurlijk van harte welkom
+om samen met Karpe noktem even uit te puffen.
+</dd>
+</dl>
 <?php default_footer(); ?>
