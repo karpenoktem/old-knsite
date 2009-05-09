d5071d7231f792edbdde8a789bf2bce65ec7482f
Bas Westerbaan <bas@fsfe.org>
2009-05-09 12:01:06 +0000

    lustrum: remove top link
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/lustrumPoster.php
+++ actions/lustrumPoster.php
@@ -2,7 +2,6 @@
 	  include_stylesheet('lustrumPoster');
 	  bare_page();
 	  default_header(); ?>
-<p class="wrappingtext">(<a href="<?php echo auri('lustrum'); ?>">ga naar de website</a>)</p>	 
 <a href="<?php echo auri('lustrum'); ?>" title="Naar de website" />
      <img src="<?php echo curi('img/lustrum5.jpg'); ?>"
           alt="Lustrum Poster" title="Lustrum Poster" 
