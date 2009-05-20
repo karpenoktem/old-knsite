68f955f23a61f59abb82c6d8cea84bb10558f8a5
Jille Timmermans <jille@karpenoktem.nl>
2009-05-20 15:10:47 +0200

    agenda: Ook de agenda op de home-page niet laten crashen

--- actions/home.php
+++ actions/home.php
@@ -1,11 +1,13 @@
 <?php use_background('home');
 	  include_stylesheet('home');
 	  require_once('lib/agenda.php');
-	  default_header(); ?>
+	  default_header();
+    if(is_array($cfg['agenda'])) {
+?>
 			<ul id="agenda">
 				<?php foreach($cfg['agenda'] as $item) { ?>
 				<li><div class="title"><a href="<?php echo auri('agenda', '', $item['key']); ?>"><?php echo $item[0]; ?></a></div>
 				    <div class="desc"><?php echo $item[1]; ?></div>
 				<?php } ?>
 			</ul>
-<?php default_footer(); ?>
+<?php } default_footer(); ?>
