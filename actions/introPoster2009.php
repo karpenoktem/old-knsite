76aa600f2ba346b3982412fe0fdbf564ed6e8074
Bas Westerbaan <bas@fsfe.org>
2009-08-13 00:48:50 +0000

    intro2009: initial version
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/introPoster2009.php
@@ -0,0 +1,10 @@
+<?php set_title('Karpe Noktem');
+	  include_stylesheet('introPoster2009');
+	  bare_page();
+	  default_header(); ?>
+<a href="<?php echo auri('intro2009'); ?>" title="Naar de website" />
+     <img src="<?php echo curi('img/introposter2009.jpg'); ?>"
+          alt="Intro Poster 2009" title="Intro Poster 2009" 
+	  class="boilerplate" /></a>
+<p class="wrappingtext">(<a href="<?php echo auri('intro2009'); ?>">ga naar de website</a>)</p>	 
+<?php default_footer(); ?>
