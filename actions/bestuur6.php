bb038484273844944a1541b5f3130f345ee49b79
Bas Westerbaan <bas@westerbaan.name>
2009-09-03 19:33:46 +0200

    bestuur6: emdash -> mdash
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- actions/bestuur6.php
+++ actions/bestuur6.php
@@ -9,10 +9,10 @@
 <p><strong>Het zesde bestuur</strong> van ASV Karpe Noktem is uit het hart
 van de vereniging gegrepen: jong, eigenzinnig en ruimdenkend.
 <em>Michiel van Lierop</em> ('88), student Informatica, loodst ons onvervaard
-door de maalstroom van het Nijmeegs studentenleven &emdash; altijd op koers
-door het houvast van <em>Bas Westerbaan</em> ('88), student Wiskunde &emdash;
+door de maalstroom van het Nijmeegs studentenleven &mdash; altijd op koers
+door het houvast van <em>Bas Westerbaan</em> ('88), student Wiskunde &mdash;
 nooit zonder middelen door, de klassiek geschoolde, <em>Hugo van Versendaal</em>
- ('85), student Grieks en Latijn &emdash; nimmer zonder bondgenoten met,
+ ('85), student Grieks en Latijn &mdash; nimmer zonder bondgenoten met,
 aller vriend, <em>Mike Lighthart</em> ('88), student Scheikende en stevig
 gehuisvest en van alle gemakken voorzien door <em>Rik Sprenkels</em> ('88),
 student Filosofie en Scheikunde.</p>
