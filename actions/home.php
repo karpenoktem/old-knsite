610c9b3ced19f2cb479b6c359e8823358535e74d
Bas Westerbaan <bas@fsfe.org>
2009-09-03 16:04:52 +0000

    home: limit agenda on home page to 8 items.
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/home.php
+++ actions/home.php
@@ -2,10 +2,19 @@
 	  include_stylesheet('home');
 	  require_once('lib/agenda.php');
 	  default_header();
-    if(is_array($cfg['agenda'])) {
-?>
+    if(is_array($cfg['agenda'])) { ?>
 			<ul id="agenda">
-				<?php foreach($cfg['agenda'] as $item) { ?>
+			<?php
+	    		$i = 0;
+			foreach($cfg['agenda'] as $item) {
+				$i++;
+				if ($i == 9) {
+					?>
+					<li><a class="more" href="<?php echo auri('agenda'); ?>">...</a></li>
+					<?php
+					break;
+				}
+			?>
 				<li><div class="title"><a href="<?php echo auri('agenda', '', $item['key']); ?>"><?php echo $item[0]; ?></a></div>
 				    <div class="desc"><?php echo $item[1]; ?></div>
 				<?php } ?>
