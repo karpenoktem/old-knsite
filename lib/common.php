4ae54b4010b944ab8016a761be92861d7ae4e504
Bas Westerbaan <bas@fsfe.org>
2009-10-27 20:45:42 +0000

    xhtml strictness: remove lang attribute on html
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- lib/common.php
+++ lib/common.php
@@ -60,7 +60,7 @@ function default_header() {
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
         "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<?php emit_stylesheets() ?>
