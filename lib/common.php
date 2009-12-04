1d21ec12508fc1093da82ecdc051395e97a0b1be
Bas Westerbaan <bas@fsfe.org>
2009-12-04 21:54:32 +0000

    lib/common: add Google Analytics
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- lib/common.php
+++ lib/common.php
@@ -167,6 +167,15 @@ function default_footer() { global $cfg, $page; ?>
 		<script type="text/javascript">
 			common_init();
 		</script>
+		<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-11922614-1");
+			pageTracker._trackPageview();
+		} catch(err) {}</script>
 	</body>
 </html> <?php
 }
