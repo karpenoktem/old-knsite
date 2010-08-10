50dfd3cfc4a373e3cbe19e3082308c67c93810f4
Bas Westerbaan <bas@fsfe.org>
2010-08-10 12:34:36 +0000

    Add intro2010 page
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

---
+++ actions/intro2010.php
@@ -0,0 +1,90 @@
+<?php set_title('Agenda intro 2010');
+	  include_stylesheet('intro2010');
+	  default_header(); ?>
+<dl id="introAgenda">
+<div class="date">maandag 16 aug</div>
+<dt>Pubquiz</dt>
+<dd>
+Na het grote succes van vorig jaar alweer een pubquiz in de intro!  Onze
+Quizmasters hebben weer een briljante quiz voor ons in petto.  Doe mee,
+test je nutteloze kennis en win die felbegeerde prijs!<br/>
+<em class="footer">20:30 in Café de Fiets, Grotestraat 8</em>
+</dd>
+
+
+<div class="date">dinsdag 17 aug</div>
+<dt>Villa Live</dt>
+<dd>
+Wij geven samen met de andere zeven verenigingen in de Villa van Schaeck een
+groot verbroederingsfeest, waarbij de live muziek niet ontbreekt!  Met twee
+zalen met verschillende DJ's is er voor ieders wil wat.<br/>
+<em class="footer">22:00 in de <a href="<?php auri('route'); ?>">Villa van Schaeck</a>.
+Entr&eacute;e &euro; 3</em>
+</dd>
+
+<div class="date">woensdag 18 aug</div>
+<dt>Karpe Intr'Rock</dt>
+<dd>
+Karpe Noktem pakt uit!  De band
+<a href="http://www.myspace.com/unlockedmemories">Unlocked Memories</a> trapt
+af met melodische nu-metal.  Daarna neemt
+<a href="http://www.myspace.com/voltviolin">Volt Violin</a>, die voor de tweede
+maal voor ons speelt, het over met sfeervolle, maar ijzersterke rock.  Na de
+bands nemen onze eigen huis DJ's het over en  garanderen wij een geweldige
+avond van jouw introductie.<br/>
+<em class="footer">21:00 in de 
+<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a>; 
+ Entr&eacute;e &euro; 3; bier, fris en wijn voor slechts één euro</em>
+</dd>
+
+<div class="date">zondag 22 aug</div>
+<dt>Spelletjesavond</dt>
+<dd>
+Na een week vol met feesten en een geweldig introductieweekend laten wij jou
+zien hoe knus en verademend een gerelaxte spelletjesavond kan zijn.  Leg
+een kaartje, speel schaak of een ander spel onder het genot van snacks,
+zachte muziek en een ontspannen gezelschap bij de Karpe Noktem spelletjesavond.
+<br/>
+<em class="footer">21:00 in de kelder van de
+<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a></em>
+</dd>
+
+<div class="date">maandag 23 aug</div>
+<dt>Anti-Kater Wandel Aktiviteit</dt>
+<dd>
+Een introductie is druk: veel nieuwe mensen, kamers en kroegen.  Karpe Noktem
+laat jou uitwaaien tijdens de AKWA, de Anti Kater Wander Acitiviteit.  Naast
+al die tenten in de binnenstad, zie je ook nog eens de mooie natuur rondom
+Nijmegen.<br/>
+<em class="footer">Verzamelen met fiets om 12:30 voor het Gymnasium</em>
+</dd>
+
+<div class="date">maandag 23 aug</div>
+<dt>BBQ</dt>
+<dd>
+Een gezellige barbecue met vlees van een biologische slager en dat proef je!
+Ook voor vega's hebben wij van alles wat.<br/>
+<em class="footer">17:00 in de achtertuin van de
+<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a></em>
+</dd>
+
+<div class="date">maandag 23 aug</div>
+<dt>Open Borrel</dt>
+<dd>
+Begeef je in nachtelijke sferen met goed gezelschap, hippe muziek, whiskey
+en speciaalbier.  Start kortom je jaar met een knal op deze spetterende
+open borrel.<br/>
+<em class="footer">21:00 in de kelder van de 
+<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a></em>
+</dd>
+
+<div class="date">dinsdag 24 aug</div>
+<dt>Nachtpicknick</dt>
+<dd>
+Trap je luie mentorpappa en -mama en maak de zonsopgang mee onder het genot
+van een prachtige picknick midden op het keizerkarelplein.<br/>
+<em class="footer">Vanaf 23:00 tot diep in de nacht op het Keizer Karel Plein</em>
+</dd>
+
+</dl>
+<?php default_footer(); ?>
