1f9ca6b72389ec972c72a6d60a98790389c636d6
Bas Westerbaan <bas@fsfe.org>
2010-08-16 11:11:03 +0000

    introPoster2010: add notice that pubquiz is moved
    Signed-off-by: Bas Westerbaan <bas@fsfe.org>

--- actions/introPoster2010.php
+++ actions/introPoster2010.php
@@ -2,6 +2,8 @@
 	  include_stylesheet('introPoster2010');
 	  bare_page();
 	  default_header(); ?>
+<div class="warning">De <strong>pubquiz</strong> maandagavond is verplaatst naar
+de kelder van caf&eacute; <strong>Tio Pepe</strong>, Bloemerstraat 82.</div>
 <a href="<?php echo auri('intro2010'); ?>" title="Naar de website" />
      <img src="<?php echo curi('img/introposter2010-3.png'); ?>"
 	  alt="Intro Poster 2010. Klik hier om verder te gaan naar de website"
