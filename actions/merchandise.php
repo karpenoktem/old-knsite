53b4dcdd646af73bd01d9f8116b5b1702735e91c
Petervdv <petervdv@karpenoktem.nl>
2010-10-29 12:49:25 +0200

    merchanise: spul->proco & voorbeelden aanpassen
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- actions/merchandise.php
+++ actions/merchandise.php
@@ -1,7 +1,7 @@
 <?php set_title('Merchandise');
 	  default_header(); ?>
-<p>Wil je zo een kek Karpe Noktem <strong>t-shirt</strong>; ben je
-geinteresseerd in een <strong>vlag</strong> of heb jij een goed idee voor
-ander Karpe Noktem spul?  E-Mail dan naar
-<?php echo email('spul@karpenoktem.nl'); ?>.</p>
+<p>Wil je zo een kek Karpe Noktem <strong>t-shirt</strong>; wil je 
+ook <strong>stickers</strong> om je omgeving onveilig te maken
+of heb jij een goed idee voor ander Karpe Noktem spul?  E-Mail dan naar
+<?php echo email('proco@karpenoktem.nl'); ?>.</p>
 <?php default_footer(); ?>
