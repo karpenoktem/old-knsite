526efa8c42f0ff5700182a704ae5d0fb065a22fd
Jille Timmermans <jille@karpenoktem.nl>
2011-01-10 15:35:53 +0000

    Updated 'bestuur7' to reflect changes in titles

--- actions/bestuur7.php
+++ actions/bestuur7.php
@@ -4,29 +4,30 @@
 	<img src="<?php echo curi('img/bestuur7.jpg');?>" />
 	<center><small><table>
 		<tr>
-			<td colspan="2" style="text-align: center; padding-right: 25px">Chaim (voorzitter),</td>
+			<td colspan="2" style="text-align: center; padding-right: 25px">Chaim (vicevoorzitter),</td>
 		</tr>
 		<tr>
-			<td style="padding-right: 25px">Bente (vice-voorzitter),</td>
+			<td style="padding-right: 25px">Bente (penningmeester),</td>
 			<td style="padding-left: 10px">Robin (pandcommissaris)</td>
 		</tr>
 		<tr>
 			<td><span style="float: right">en</span>Jille (secretaris)</td>
-			<td style="padding-left: 25px">Remco (penningmeester).</td>
+			<td style="padding-left: 25px">Remco (voorzitter).</td>
 		</tr>
 	</table></small></center><br>
 </p>
 <p><strong>Het zevende bestuur</strong> van ASV Karpe Noktem bestaat
 uit 5 enthousiaste leden die een breed spectrum van zijn vereniging
-vertegenwoordigen. <em>Chaim van Rens</em> ('85, voorzitter), student leraar
-geschiedenis, de vertegenwoordiging van het HBO - ondersteund door
-de digitale kennis van <em>Jille Timmermans</em> ('90, secretaris), student
-Informatica - vergezeld door een die zelfs bij de kleinste zaken scherp
-blijft, <em>Remco van Cruchten</em> ('90, penningmeester), student moleculaire
-levenswetenschappen - dat niet onder doet voor het vrouwelijke deel van
-ons bestuur, vertegenwoordigd door <em>Bente Rotberg</em> ('90, vice-voorzitter)
-studente sociologie en, voor alle communicatie binnen de Villa van Schaeck,
+vertegenwoordigen. <em>Remco van Cruchten</em> ('90, voorzitter), student
+moleculare levenswetenschappen, houdt de touwtjes strak en wordt daarbij
+ondersteund door de digitale kennis van <em>Jille Timmermans</em> ('90,
+secretaris), student informatica, en geholpen door een die zelfs bij de
+kleinste zaken scherp blijft, <em>Bente Rotberg</em> ('90, penningmeester),
+studente sociologie, en bijgestaan door <em>Chaim van Rens</em> ('85,
+vicevoorzitter), student leraar geschiedenis, de vertegenwoordiging van het HBO
+en, voor alle communicatie binnen de Villa van Schaeck,
 <em>Robin Nazarski</em> ('89, pandcommissaris), studente geschiedenis.</p>
+
 <p>&lArr; Het vorige,
 <a href="<?php echo auri('bestuur6'); ?>">zesde bestuur</a>.</p>
 <?php default_footer(); ?>
