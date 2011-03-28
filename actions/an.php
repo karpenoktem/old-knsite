2b861099742bc924d977e220e9e402fedc858a45
Bas Westerbaan <bas@westerbaan.name>
2011-03-28 23:22:09 +0200

    Akta 23
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- actions/an.php
+++ actions/an.php
@@ -8,6 +8,7 @@
 			<li><a href="<?php echo curi('archief/akta20.pdf'); ?>">(Intro) Akta 20</a></li>
 			<li><a href="<?php echo curi('archief/akta21.pdf'); ?>">Akta 21</a></li>
 			<li><a href="<?php echo curi('archief/akta22.pdf'); ?>">Akta 22</a></li>
+			<li><a href="<?php echo curi('archief/akta23.pdf'); ?>">Akta 23</a></li>
 		</ul>
 		<li>Jaargang 6</li>
 		<ul>
