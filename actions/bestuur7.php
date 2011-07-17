593088769ad0d6fef62fb68bbd728722a0504eb0
Jille Timmermans <jille@karpenoktem.nl>
2011-07-17 18:41:52 +0000

    Modifications to text of the bestuur7 page
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/bestuur7.php
+++ actions/bestuur7.php
@@ -17,7 +17,7 @@
 	</table></small></center><br>
 </p>
 <p><strong>Het zevende bestuur</strong> van ASV Karpe Noktem bestaat
-uit 5 enthousiaste leden die een breed spectrum van zijn vereniging
+uit 5 enthousiaste leden die een breed spectrum van hun vereniging
 vertegenwoordigen. <em>Remco van Cruchten</em> ('90, voorzitter), student
 moleculare levenswetenschappen, houdt de touwtjes strak en wordt daarbij
 ondersteund door de digitale kennis van <em>Jille Timmermans</em> ('90,
@@ -27,6 +27,8 @@ studente sociologie, en bijgestaan door <em>Chaim van Rens</em> ('85,
 vicevoorzitter), student leraar geschiedenis, de vertegenwoordiging van het HBO
 en, voor alle communicatie binnen de Villa van Schaeck,
 <em>Robin Nazarski</em> ('89, pandcommissaris), studente geschiedenis.</p>
+
+<p>Alle bestuursleden zijn bereikbaar op hun functie- of voornaam @karpenoktem.nl.</p>
 
 <p>&lArr; Het vorige,
 <a href="<?php echo auri('bestuur6'); ?>">zesde bestuur</a>.</p>
