d24c775a1ae14de78cd63dea00146377e2bb59cc
petervdv <peter.901@gmail.com>
2011-09-28 12:19:11 +0300

    Dode links weg, geactualiseerd

--- actions/media.php
+++ actions/media.php
@@ -1,21 +1,15 @@
 <?php set_title('Media');
       default_header(); ?>
+
 <ul>
-	<li><strong>Nieuw</strong> de
-	<a href="http://www.youtube.com/watch?v=NLOT1TkZTf4">video</a>
-	van campustv van onze nachtpicknick.</li>
-	<li><a href="http://karpenoktem.nl/fotos/"
-	     >Fotos van Karpe Noktem</a> en</li>
-	<li><a href="http://karpenoktem.nl/fotos/index.php?album=pdn"
-	     >Pluk de Nacht weekend</a> .</li>
-	<li><a href="http://www.carolusmagnus.nl/page_types/photogallery/show_collection.asp?collection_id=116"
-	     >Fotos op de site van Carolus</a>; videos 
-	    <a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem"
-	    >op het voxlog</a> en</li>
-	    <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142"
-	    >op CampusTV</a> van het <em>benefiet feest</em> gegeven samen met
-	    <a href="http://www.carolusmagnus.nl/">NSV Carolus Magnus</a>
-	    in de <a href="http://www.ru.nl/ragweek/">ragweek</a>
-	    van 2008.</li>
+	<li>Foto's van <a href="http://karpenoktem.nl/fotos/">Karpe Noktem</a></li>
+	
+	<li>Foto's van <a href="http://karpenoktem.nl/fotos/index.php?album=pdn">Pluk de Nacht weekend</a></li>
+	
+	<li>2009: Een reportage van <a href="http://www.youtube.com/watch?v=NLOT1TkZTf4">Campus in Beeld</a> tijdens onze nachtpicknick</li>
+	
+	<li>2008: een reportage van <a href="http://www.voxlog.nl/blog/studentslive-carolus-magnus-karpe-noktem">Voxlog</a> en <a href="http://www.campus.tv/index.php?action=showmovie&movieid=1142">CampusTV</a> over het Tussen Twee Werelden Feest met Carolus Magnus tijdens de Ragweek</li>
+
 </ul>
+
 <?php default_footer(); ?>
