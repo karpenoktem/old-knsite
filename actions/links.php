ea47794ffe27cc46292afd26db26c6b9ac764ac2
petervdv <peter.901@gmail.com>
2011-09-28 13:24:29 +0300

    Geactualiseerd

--- actions/links.php
+++ actions/links.php
@@ -1,24 +1,27 @@
 <?php set_title('Links');
       default_header(); ?>
-	<p>Karpe Noktem op</p>
-	<ul>
-                <li><a href="http://www.facebook.com/karpenoktem"
-                        >Facebook</a>,</li>
-                <li><a href="http://twitter.com/karpenoktem"
-                        >Twitter</a>,</li>
-		<li><a href="http://www.last.fm/group/Karpe+Noktem">Last.fm</a>,</li>
-		<li><a href="http://karpenoktem.hyves.nl/">Hyves</a>,</li>
-		<li><a href="http://www.youtube.com/KarpeNoktem"
-		>YouTube</a> (video's),</li>
-		<li><a href="http://www.flickr.com/photos/12267979@N04/"
-		>Flickr</a> (oude foto's: <a href="http://karpenoktem.nl/fotos"
-		>hier</a> staan de nieuwe).</li>
-	</ul>
-	<p>Maar vergeet niet onze</p>
-	<ul>
-                <li><a href="<?php echo auri('zusjes') ?>"
-                        >zusjes</a>,</li>
-		<li><a href="<?php echo auri('forum') ?>">forum</a> en</li>
-		<li>onze <a href="http://karpenoktem.nl/wiki">wiki</a>.</li>
-	</ul>
+
+    <p>Karpe Noktem op platforms</p> 
+    
+    <ul> 
+      <li><a href="http://www.facebook.com/karpenoktem">Facebook</a></li> 
+      <li><a href="http://twitter.com/karpenoktem">Twitter</a></li> 
+      <li><a href="http://www.last.fm/group/Karpe+Noktem">Last.fm</a></li> 
+    </ul> 
+    
+    <p>Nog meer Karpe Noktem</p> 
+    
+    <ul> 
+      <li><a href="http://karpenoktem.nl/wiki">wiki</a></li> 
+      <li><a href="%20<?php echo auri('forum') ?>%20">forum</a></li> 
+      <li><a href="%20<?php echo auri('zusjes') ?>%20">zusjes</a></li> 
+    </ul> 
+    
+    <p>Een stukje historie</p> 
+    
+    <ul> 
+      <li><a href="http://karpenoktem.hyves.nl/">Hyves</a></li> 
+      <li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr</a> (nieuwe foto's op onze <a href="%20<?php echo auri('fotos') ?>%20">eigen website</a>)</li> 
+    </ul>
+  
 <?php default_footer(); ?>
