95363973a4ebae0ff9a4c03b2522e748b5a6d309
Bas Westerbaan <bas@westerbaan.name>
2011-10-01 13:44:40 +0200

    links: style fixes
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- actions/links.php
+++ actions/links.php
@@ -1,27 +1,25 @@
 <?php set_title('Links');
       default_header(); ?>
+<p>Karpe Noktem op</p>
+<ul>
+<li><a href="http://www.facebook.com/karpenoktem">Facebook</a>,</li>
+<li><a href="http://twitter.com/karpenoktem">Twitter</a> en</li>
+<li><a href="http://www.last.fm/group/Karpe+Noktem">Last.fm</a>.</li>
+</ul>
 
-    <p>Karpe Noktem op platforms</p> 
-    
-    <ul> 
-      <li><a href="http://www.facebook.com/karpenoktem">Facebook</a></li> 
-      <li><a href="http://twitter.com/karpenoktem">Twitter</a></li> 
-      <li><a href="http://www.last.fm/group/Karpe+Noktem">Last.fm</a></li> 
-    </ul> 
-    
-    <p>Nog meer Karpe Noktem</p> 
-    
-    <ul> 
-      <li><a href="http://karpenoktem.nl/wiki">wiki</a></li> 
-      <li><a href="%20<?php echo auri('forum') ?>%20">forum</a></li> 
-      <li><a href="%20<?php echo auri('zusjes') ?>%20">zusjes</a></li> 
-    </ul> 
-    
-    <p>Een stukje historie</p> 
-    
-    <ul> 
-      <li><a href="http://karpenoktem.hyves.nl/">Hyves</a></li> 
-      <li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr</a> (nieuwe foto's op onze <a href="%20<?php echo auri('fotos') ?>%20">eigen website</a>)</li> 
-    </ul>
-  
+<p>Wij hebben ons ook begeven op</p>
+<ul>
+<li><a href="http://karpenoktem.hyves.nl/">Hyves</a> en</li>
+<li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr</a> (kijk
+voor de nieuwe foto's op
+<a href="<?php echo auri('fotos') ?>">onze eigen site</a>).</li>
+</ul>
+
+<p>Maar vergeet niet</p>
+<ul>
+<li><a href="http://karpenoktem.nl/wiki">onze wiki</a>,</li>
+<li><a href="<?php echo auri('forum') ?>">ons forum</a> en</li>
+<li>de <a href="<?php echo auri('zusjes') ?>">zusjes</a>.</li>
+</ul>
+
 <?php default_footer(); ?>
