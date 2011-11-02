0e512ee3207bb5d5ab807b6accb9cf31e569049a
Bram Westerbaan <bram@westerbaan.name>
2011-11-03 00:25:22 +0100

    baragenda -> planning

--- actions/baragenda.php
+++ actions/baragenda.php
@@ -1,13 +1,3 @@
-<?php set_title('Bar Agenda');
-	  include_stylesheet('agenda');
-	  require_once('config.baragenda.php');
-	  default_header(); ?>
-<ul id="fullagenda">
-	<?php foreach($cfg['baragenda'] as $item) { ?>
-	<li><a name="<?php echo $item['key']; ?>"></a>
-	<div class="date"><?php echo $item[2]; ?></div>
-	<div class="name"><?php echo $item[3]; ?></div>
-	<div class="desc"><?php echo $item[4]; ?></div></li>
-	<?php } ?>
-</ul>
-<?php default_footer(); ?>
+<?php
+header("location: /planning",True,301);
+?>
