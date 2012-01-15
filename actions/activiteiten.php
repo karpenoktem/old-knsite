27292817fb4b791b22415dd6cb2173837b684120
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 20:56:59 +0100

    Text van de activiteitenpagina een beetje bijgewerkt
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/activiteiten.php
+++ actions/activiteiten.php
@@ -6,10 +6,7 @@ bandjesavonden. Daarnaast programmeren we voor leden tal van andere
 activiteiten. Een kleine greep uit voorgaande hoogtepunten: Karpe Rockt'em, De
 nacht van de po&euml;zie, Het jaarlijkse gemaskerd bal &amp; Karpe Noktem
 weekend.
-Ook verzeil je regelmatig in een vreemde stad tijdens onze Roadtrips en feesten
-bij onze zusterverenigingen van het ZEUS-verband.</p>
-<p>Natuurlijk is er elke maandag een gezellige borrel in onze vaste stek, de
-<a href="<?php echo auri('route'); ?>">Villa van Schaeck</a>.  Wil je concrete 
-up-to-date informatie over ons programma? Kijk even in
-onze <a href="<?php echo auri('agenda') ?>">agenda</a>.</p>
+Ook verzeil je regelmatig in een vreemde stad tijdens onze roadtrips en feesten
+bij onze <a href="<?php echo auri('zusjes'); ?>">zusterverenigingen</a> van het ZEUS-verband.</p>
+<p>We hebben elke maandag een vaste borrel in de <a href="<?php echo auri('contact', '', 'route'); ?>">Villa van Schaeck</a> en elke vrijdag een door &eacute;&eacute;n van onze vele commissies neergezette activiteit.</p> Wil je concrete, up-to-date informatie over ons programma? Kijk even in onze <a href="<?php echo auri('agenda') ?>">agenda</a>.</p>
 <?php default_footer(); ?>
