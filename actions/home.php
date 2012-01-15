bc6195aeef179732035f46cf34c86aae845deb5b
Felix Janssen <felix@karpenoktem.nl>
2012-01-15 18:59:27 +0100

    * breedte naar 900 px teruggebracht * homepage content gegeven * consistentie-fixes

--- actions/home.php
+++ actions/home.php
@@ -2,7 +2,17 @@
 	  include_stylesheet('home');
 	  require_once('lib/agenda.php');
 	  default_header();
+?>
+	  <div id="homeContent">
+	  	<img src="<?php echo curi('img/dansend.jpg'); ?>" width="475" />
+		<p>
+	  		Welkom bij ASV Karpe Noktem. Hier staat wat vrolijke tekst waarin kort uitleg wordt gegeven over wat KN is. En een mooie foto waaraan je in 1 keer ziet dat KN cool is.
+		</p>
+	  </div>
+<?php
     if(is_array($cfg['agenda'])) { ?>
+			<div id="agenda">
+			<h1>Agenda</h1>
 			<ul id="agenda">
 			<?php
 	    		$i = 0;
@@ -19,4 +29,5 @@
 				    <div class="desc"><?php echo $item[1]; ?></div></li>
 				<?php } ?>
 			</ul>
+			</div>
 <?php } default_footer(); ?>
