225ed5df654355b2b6f166ee4814268f3817acaf
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 17:56:42 +0100

    Route samengevoegd met contact
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/contact.php
+++ actions/contact.php
@@ -15,4 +15,16 @@ u sturen naar:</p>
 <blockquote>ASV Karpe Noktem<br/>
 Postbus 1221<br/>
 6501BE Nijmegen </blockquote>
+
+<a name="route" />
+<p>Karpe Noktem is gevestigd in de <a href="http://www.villavanschaeck.nl/">Villa van Schaeck</a>. Daar is ons secretariaat en zijn ook de meeste activiteiten.
+De Villa ligt rechts aan de directe weg van het Keizer Karelplein naar het Centraal Station.</p>
+<address>Villa van Schaeck<br/>
+Van Schaeck Mathonsingel 10<br/>
+6512AP Nijmegen</address>
+<p>
+<iframe width="425" height="350" frameborder="0" scrolling="no" 
+        marginheight="0" marginwidth="0" 
+	src="http://maps.google.com/?ie=UTF8&amp;t=h&amp;s=AARTsJqzARj-Z8VnW5pkPMLMmZbqrJcYpw&amp;ll=51.842718,5.85641&amp;spn=0.00232,0.00456&amp;z=17&amp;output=embed"></iframe><br />
+<small><a href="http://maps.google.com/?ie=UTF8&amp;t=h&amp;ll=51.842718,5.85641&amp;spn=0.00232,0.00456&amp;z=17&amp;source=embed" style="text-align:left">Grotere kaart</a></small>
 <?php default_footer(); ?>
