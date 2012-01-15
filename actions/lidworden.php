869877d63099841157eb4e1aa9264eb727c26df9
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 21:37:30 +0100

    Contact en lid-worden pagina wat aangepast
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/lidworden.php
+++ actions/lidworden.php
@@ -5,4 +5,5 @@
 borrels in <a href="<?php echo auri('route'); ?>">de Villa van Schaeck</a>, of stuur 
 een e-mailtje naar 
 <?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
+<p>Je kunt natuurlijk ook eerst gewoon nog een paar keer komen kijken om de sfeer op onze activiteiten mee te krijgen. Loop dan een keertje binnen op een maandagavond of maak een afspraak met iemand van <?php echo email('welcom@karpenoktem.nl'); ?>, die graag een keer met je afspreekt om je bijv. van het station op te halen.
 <?php default_footer(); ?>
