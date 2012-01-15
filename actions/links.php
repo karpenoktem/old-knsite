6115d43d11516b019f4dd34aedb9de09d4687e7f
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 17:57:12 +0100

    Links pagina bijgewerkt
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/links.php
+++ actions/links.php
@@ -1,25 +1,21 @@
 <?php set_title('Links');
       default_header(); ?>
-<p>Karpe Noktem op</p>
+<p>Karpe Noktem is te vinden op</p>
 <ul>
 <li><a href="http://www.facebook.com/karpenoktem">Facebook</a>,</li>
 <li><a href="http://twitter.com/karpenoktem">Twitter</a> en</li>
 <li><a href="http://www.last.fm/group/Karpe+Noktem">Last.fm</a>.</li>
 </ul>
 
-<p>Wij hebben ons ook begeven op</p>
-<ul>
-<li><a href="http://karpenoktem.hyves.nl/">Hyves</a> en</li>
-<li><a href="http://www.flickr.com/photos/12267979@N04/">Flickr</a> (kijk
-voor de nieuwe foto's op
-<a href="<?php echo auri('fotos') ?>">onze eigen site</a>).</li>
-</ul>
+<p>Wij hebben ons ook begeven op <a href="http://karpenoktem.hyves.nl/">Hyves</a> en <a href="http://www.flickr.com/photos/12267979@N04/">Flickr</a> <small>(kijk
+voor de nieuwe foto's op <a href="<?php echo auri('fotos') ?>">onze eigen site</a>)</small>.</p>
 
 <p>Maar vergeet niet</p>
 <ul>
 <li><a href="http://karpenoktem.nl/wiki">onze wiki</a>,</li>
-<li><a href="<?php echo auri('forum') ?>">ons forum</a> en</li>
-<li>de <a href="<?php echo auri('zusjes') ?>">zusjes</a>.</li>
+<li><a href="<?php echo auri('forum') ?>">ons forum</a>,</li>
+<li>de <a href="<?php echo auri('zusjes') ?>">zusjes</a> en </li>
+<li><a href="<?php echo auri('sponsoren') ?>">onze sponsoren</a>.</li>
 </ul>
 
 <?php default_footer(); ?>
