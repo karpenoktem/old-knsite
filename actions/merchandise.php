d150687fec0c580cf826e2f76b2ceffd0e4f8cee
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 18:09:53 +0100

    Merchandise-pagina zit ook niet meer in het menu
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- actions/merchandise.php
+++
@@ -1,7 +0,0 @@
-<?php set_title('Merchandise');
-	  default_header(); ?>
-<p>Wil je zo een kek Karpe Noktem <strong>t-shirt</strong>; wil je 
-ook <strong>stickers</strong> om je omgeving onveilig te maken
-of heb jij een goed idee voor ander Karpe Noktem spul?  E-Mail dan naar
-<?php echo email('proco@karpenoktem.nl'); ?>.</p>
-<?php default_footer(); ?>
