9109af68ebc044abaeb6f7ade9c401c37a09abe1
Jille Timmermans <jille@karpenoktem.nl>
2012-01-15 17:58:20 +0100

    Placeholder Pluk de Nacht pagina gemaakt
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

---
+++ actions/plukdenacht.php
@@ -0,0 +1,4 @@
+<?php set_title('Pluk de Nacht');
+      default_header(); ?>
+<p>Hier komt nog wat informatie over Pluk de Nacht en ook een link naar het <a href="<?php echo auri('fotos/pdn/'); ?>">fotoboek</a>.</p>
+<?php default_footer(); ?>
