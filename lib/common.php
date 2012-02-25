5406afa42f5f01cee4e6d257b301edf6be42eab4
Bas Westerbaan <bas@westerbaan.name>
2012-02-25 14:15:00 +0100

    It's 2012!
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- lib/common.php
+++ lib/common.php
@@ -156,7 +156,7 @@ function default_footer() { global $cfg, $page; ?>
 		Gebruik <a href="http://www.mozilla.com/en-US/firefox/">een</a>
 		open browser voor een beter resultaat.<br/>
 		<![endif]-->
-		&copy;2007&mdash;2011, Karpe Noktem<?php
+		&copy;2007&mdash;2012, Karpe Noktem<?php
 		if(isset($cfg['release'])) {
 			echo "; <a href='".auri('release')."'>".
 			     strftime('%e %b' ,$cfg['release']['date'])."</a>";
