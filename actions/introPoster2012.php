c31abd56bbef0953c452a01fc923654c3b5f0d04
Bas Westerbaan <bas@westerbaan.name>
2012-08-10 17:31:55 +0200

    Add introPoster2012
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ actions/introPoster2012.php
@@ -0,0 +1,12 @@
+<?php set_title('Karpe Noktem');
+	  include_stylesheet('introPoster2012');
+	  bare_page();
+	  default_header(); ?>
+<a href="<?php echo auri('home'); ?>" title="Naar de website" />
+     <img src="<?php echo curi('img/introposter2012.jpg'); ?>"
+	  alt="Intro Poster 2012. Klik hier om verder te gaan naar de website"
+	  title="Intro Poster 2012" 
+	  class="boilerplate" /></a>
+<p class="wrappingtext">(<a href="<?php echo auri('home'); ?>"
+	>ga naar de website</a>)</p>	 
+<?php default_footer(); ?>
