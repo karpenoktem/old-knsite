586c6bf623cc86b1b35f8555f6f9a8e3758f8e6c
Daan Sprenkels <dsprenkels@gmail.com>
2013-01-06 14:12:06 +0100

    Informatie voor lid-af worden op de site gezet.

--- actions/lidworden.php
+++ actions/lidworden.php
@@ -5,4 +5,8 @@
 borrels in <a href="<?php echo auri('route'); ?>">de Villa van Schaeck</a>, of stuur 
 een e-mailtje naar 
 <?php echo email('secretaris@karpenoktem.nl'); ?> en dan is het zo geregeld!</p>
+
+<h2>Lid-af worden</h2>
+<p>Wil je geen lid meer zijn? Stuur dan ook een mailtje naar <?php echo email('secretaris@karpenoktem.nl'); ?>.</p>
+
 <?php default_footer(); ?>
