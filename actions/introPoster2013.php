e641cc85793130b5d24bcf6a7cb10bbf5f826600
Bas Westerbaan <bas@westerbaan.name>
2013-08-09 19:16:16 +0200

    introPoster2013

---
+++ actions/introPoster2013.php
@@ -0,0 +1,12 @@
+<?php set_title('Karpe Noktem');
+	  include_stylesheet('introPoster2013');
+	  bare_page();
+	  default_header(); ?>
+<a href="<?php echo auri('home'); ?>" title="Naar de website" />
+     <img src="<?php echo curi('img/introposter2013.jpg'); ?>"
+	  alt="Intro Poster 2013. Klik hier om verder te gaan naar de website"
+	  title="Intro Poster 2013" 
+	  class="boilerplate" /></a>
+<p class="wrappingtext">(<a href="<?php echo auri('home'); ?>"
+	>ga naar de website</a>)</p>	 
+<?php default_footer(); ?>
