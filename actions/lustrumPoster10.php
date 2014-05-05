ac66c5fc2f83b5ae525c166d858a39f83218df02
Bas Westerbaan <bas@westerbaan.name>
2014-05-05 12:44:12 +0200

    Voeg lustrumPoster10 toe, en hernoem de oude naar lustrumPoster5

---
+++ actions/lustrumPoster10.php
@@ -0,0 +1,11 @@
+<?php set_title('Lustrum');
+	  include_stylesheet('lustrumPoster10');
+	  bare_page();
+	  default_header(); ?>
+<a href="<?php echo auri('home'); ?>" title="Naar de website" />
+     <img src="<?php echo curi('img/lustrum10.jpg'); ?>"
+          alt="Lustrum Poster" title="Lustrum Poster"
+          width="600" 
+	  class="boilerplate" /></a>
+<p class="wrappingtext">(<a href="<?php echo auri('home'); ?>">ga naar de website</a>)</p>	 
+<?php default_footer(); ?>
---
+++ actions/lustrumPoster5.php
@@ -0,0 +1,10 @@
+<?php set_title('Lustrum');
+	  include_stylesheet('lustrumPoster5');
+	  bare_page();
+	  default_header(); ?>
+<a href="<?php echo auri('lustrum'); ?>" title="Naar de website" />
+     <img src="<?php echo curi('img/lustrum5.jpg'); ?>"
+          alt="Lustrum Poster" title="Lustrum Poster" 
+	  class="boilerplate" /></a>
+<p class="wrappingtext">(<a href="<?php echo auri('lustrum'); ?>">ga naar de website</a>)</p>	 
+<?php default_footer(); ?>
